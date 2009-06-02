2e76c415e1e75a1badaacc056aa2674c82cf6b3e
Christian Parpart <trapni@gentoo.org>
2009-06-02 13:37:48 +0200

    added severity named enum

--- src/x0/types.hpp
+++ src/x0/types.hpp
@@ -37,6 +37,26 @@ typedef boost::shared_ptr<connection> connection_ptr;
 typedef boost::shared_ptr<response> response_ptr;
 
 /**
+ * \ingroup common
+ * \brief named enum `severity`, used by logging facility
+ * \see logger
+ */
+struct severity {
+	static const int emergency = 0;
+	static const int alert = 1;
+	static const int critical = 2;
+	static const int error = 3;
+	static const int warn = 4;
+	static const int notice = 5;
+	static const int info = 6;
+	static const int debug = 7;
+
+	int value_;
+	severity(int value) : value_(value) { }
+	operator int() const { return value_; }
+};
+
+/**
  * \ingroup core
  * \brief request handler functor.
  */
