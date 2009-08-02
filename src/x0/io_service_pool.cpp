04bedaaa20cc00a1a756f488d6453e22ff1b5af2
Christian Parpart <trapni@gentoo.org>
2009-08-02 03:08:14 +0200

    core: always have at least one io_service up, even when setup() hasn't been called *yet*.

--- src/x0/io_service_pool.cpp
+++ src/x0/io_service_pool.cpp
@@ -15,11 +15,17 @@ namespace x0 {
 io_service_pool::io_service_pool() :
 	services_(), works_(), next_(0)
 {
+	// always have at least one service around
+	io_service_ptr service(new boost::asio::io_service);
+	work_ptr work(new boost::asio::io_service::work(*service));
+
+	services_.push_back(service);
+	works_.push_back(work);
 }
 
 void io_service_pool::setup(std::size_t num_threads)
 {
-	for (std::size_t i = 0; i < num_threads; ++i)
+	for (std::size_t i = 1; i < num_threads; ++i)
 	{
 		io_service_ptr service(new boost::asio::io_service);
 		services_.push_back(service);
