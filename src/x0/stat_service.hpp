ef98db288c618bcb0f6085f8dacf24d7d7dc86d7
Christian Parpart <trapni@gentoo.org>
2009-08-02 15:20:35 +0200

    stat_service is working insie x0 now, too. *yippie*
    Remember, it's still not (yet?) thread-safe.
    I need to rethink whether and how to handle that.
    Maybe in combination with strand's and/or TLS's.

--- src/x0/stat_service.hpp
+++ src/x0/stat_service.hpp
@@ -11,6 +11,12 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#if 0
+#	define STAT_DEBUG(msg...) printf(msg)
+#else
+#	define STAT_DEBUG(msg...) /*!*/
+#endif
+
 namespace x0 {
 
 /** service for retrieving file status.
@@ -18,6 +24,8 @@ namespace x0 {
  * This is like stat(), in fact, it's using stat() but
  * caches the result for further use and also invalidates in realtime
  * in case their underlying inode has been updated.
+ *
+ * \note this class is not thread-safe
  */
 class stat_service :
 	public boost::noncopyable
@@ -26,7 +34,10 @@ private:
 	boost::asio::posix::stream_descriptor in_;	//!< inotify handle used for invalidating updated stat entries
 	cache<std::string, struct stat> cache_;		//!< cache, storing path->stat pairs
 	std::map<int, std::string> wd_;				//!< stores wd->path/stat pairs - if someone knows how to eliminate this extra map, tell me.
+	boost::array<char, 8192> inbuf_;
 	inotify_event ev_;							//!< used to store a new inotify event
+	bool caching_;								//!< true if caching should be used, false otherwise
+	struct stat st_;							//!< fallback stat struct when caching is disabled.
 
 public:
 	stat_service(boost::asio::io_service& io, std::size_t mxcost = 128);
@@ -35,6 +46,9 @@ public:
 	struct stat *query(const std::string& filename);
 	struct stat *operator()(const std::string& filename);
 	int operator()(const std::string& filename, struct stat *st);
+
+	bool caching() const;
+	void caching(bool);
 
 	std::size_t size() const;
 	bool empty() const;
@@ -45,32 +59,31 @@ public:
 	void max_cost(std::size_t value);
 
 private:
+	void async_read();
+
 	void invalidate(const boost::system::error_code& ec, std::size_t bytes_transferred)
 	{
-		printf("stat.invalidate(%s, %ld)\n", ec.message().c_str(), bytes_transferred);
+		STAT_DEBUG("stat.invalidate(ec=%s, bt=%ld)\n", ec.message().c_str(), bytes_transferred);
+		inotify_event *ev = reinterpret_cast<inotify_event *>(inbuf_.data());
+		inotify_event *ee = ev + bytes_transferred;
 
-//		if (!bytes_transferred)
-//		{
-//			::read(in_.native(), &ev_, sizeof(ev_)); // use read() instead of asio's std::readv()
-//			perror("::read: ");
-//		}
-
+		while (ev < ee && ev->wd != 0)
 		{
-			auto i = wd_.find(ev_.wd);
+			STAT_DEBUG("--stat.invalidate(len=%d, fn=%s)\n", ev->len, ev->name);
+			auto i = wd_.find(ev->wd);
 
 			if (i != wd_.end())
 			{
-				printf("state.invalidate: wd: %d, remove: %s\n", ev_.wd, i->second.c_str());
+				STAT_DEBUG("--stat.invalidate: wd: %d, remove: %s\n", ev->wd, i->second.c_str());
 				cache_.remove(i->second);
 				wd_.erase(i);
 			}
+			ev += sizeof(*ev) + ev->len;
 		}
 
 		if (!ec)
 		{
-			printf("stat: re-assign async_read (jobs=%ld)\n", size());
-			boost::asio::async_read(in_, boost::asio::buffer(&ev_, sizeof(ev_)),
-				boost::bind(&stat_service::invalidate, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+			async_read();
 		}
 	}
 };
@@ -78,9 +91,17 @@ private:
 inline stat_service::stat_service(boost::asio::io_service& io, std::size_t mxcost) :
 	in_(io, ::inotify_init1(IN_NONBLOCK | IN_CLOEXEC)),
 	cache_(mxcost),
-	wd_()
+	wd_(),
+	caching_(true)
 {
-	boost::asio::async_read(in_, boost::asio::buffer(&ev_, sizeof(ev_)),
+	async_read();
+}
+
+inline void stat_service::async_read()
+{
+	STAT_DEBUG("stat: (re-)assign async_read (watches=%ld)\n", size());
+	boost::asio::async_read(in_, boost::asio::buffer(inbuf_),
+		boost::asio::transfer_at_least(sizeof(inotify_event)),
 		boost::bind(&stat_service::invalidate, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
 }
 
@@ -90,10 +111,18 @@ inline stat_service::~stat_service()
 
 inline struct stat *stat_service::query(const std::string& _filename)
 {
+	if (!caching_)
+	{
+		if (::stat(_filename.c_str(), &st_) == 0)
+			return &st_;
+
+		return 0;
+	}
+
 	std::string filename(_filename[_filename.size() - 1] == '/' ? _filename.substr(0, _filename.size() - 1) : _filename);
 	if (struct stat *st = cache_(filename))
 	{
-		printf("stat.query.cached(%s)\n", filename.c_str());
+		STAT_DEBUG("stat.query.cached(%s)\n", filename.c_str());
 		return st;
 	}
 
@@ -108,14 +137,14 @@ inline struct stat *stat_service::query(const std::string& _filename)
 
 			if (rv != -1)
 			{
-				printf("stat.query(%s) wd=%d\n", filename.c_str(), rv);
+				STAT_DEBUG("stat.query(%s) wd=%d\n", filename.c_str(), rv);
 				wd_[rv] = filename;
 			}
 
 			return st;
 		}
 	}
-	printf("stat.query(%s) failed (%s)\n", filename.c_str(), strerror(errno));
+	STAT_DEBUG("stat.query(%s) failed (%s)\n", filename.c_str(), strerror(errno));
 	// either ::stat() or caching failed.
 
 	delete st;
@@ -136,6 +165,21 @@ inline int stat_service::operator()(const std::string& filename, struct stat *_s
 		return 0;
 	}
 	return -1;
+}
+
+inline bool stat_service::caching() const
+{
+	return caching_;
+}
+
+inline void stat_service::caching(bool value)
+{
+	caching_ = value;
+
+	if (!caching_)
+	{
+		cache_.clear();
+	}
 }
 
 inline std::size_t stat_service::size() const
