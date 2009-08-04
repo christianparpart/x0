243736b7f43b560833989580a18185cc872afa25
Christian Parpart <trapni@gentoo.org>
2009-08-04 23:55:58 +0200

    silence autogen.sh!

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 if [ "$1" == "clean" ]; then
-	find . -name 'CMakeCache.txt' -print | xargs rm -vrf
-	find . -name 'CMakeFiles*' -print | xargs rm -vrf
+	find . -name 'CMakeCache.txt' -print | xargs rm -vrf &>/dev/null
+	find . -name 'CMakeFiles*' -print | xargs rm -vrf &>/dev/null
 else
 	cmake .
 fi
