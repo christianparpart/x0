493b4f7d2a4865bb09fcb7d77f6d8caaf8f9321e
Christian Parpart <trapni@gentoo.org>
2009-08-05 02:08:47 +0200

    x0d: added process guard - needs *more* testing
    todo: remove debug prints, make it overall a bit nicer ;)

--- src/x0/main.cpp
+++ src/x0/main.cpp
@@ -9,9 +9,12 @@
 #include <x0/server.hpp>
 #include <x0/strutils.hpp>
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include <iostream>
 #include <string>
 #include <getopt.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 class x0d
 {
@@ -34,6 +37,7 @@ public:
 	x0d() :
 		configfile_(pathcat(SYSCONFDIR, "x0d.conf")),
 		nofork_(false),
+		doguard_(false),
 		server_()
 	{
 		instance_ = this;
@@ -55,14 +59,108 @@ public:
 				daemonize();
 			}
 
-			::signal(SIGHUP, &reload_handler);
-			::signal(SIGTERM, &terminate_handler);
-			::signal(SIGPIPE, SIG_IGN);
-
-			server_.run();
-			return 0;
+			if (doguard_)
+			{
+				return guard(boost::bind(&x0d::_run, this));
+			}
+			else
+			{
+				return _run();
+			}
 		}
 		return -1;
+	}
+
+	template<class Handler>
+	int guard(Handler handler)
+	{
+#if 1
+		// TODO: fork and guard the child process, that is, restart the child on each time it ends abnormally
+		for (;;)
+		{
+			pid_t pid = fork();
+
+			if (pid < 0) // fork failed
+			{
+				perror("fork");
+				// TODO: syslog(strerror(errno))
+				return 1;
+			}
+			else if (pid == 0) // in child
+			{
+				return handler();
+			}
+			// in parent with a valid child
+			int rv, status = 0;
+
+			for (;;) // loop as long as waitpid() returned errno (EINTR, ...) or the program exited normally (exit(), main() leaving, ...)
+			{
+				DEBUG("Waiting on pid %d", pid);
+#if 0
+				rv = waitpid(pid, &status, 0);
+#else
+				do rv = waitpid(pid, &status, 0);
+				while (rv == -1 && errno == EINTR);
+#endif
+				perror("waitpid");
+
+				if (WIFEXITED(status))
+				{
+					DEBUG("Guarded process terminated with return code %d", WEXITSTATUS(status));
+					return WEXITSTATUS(status);
+				}
+				else if (WIFSIGNALED(status))
+				{
+					DEBUG("Guarded process terminated with signal %s", sig2str(WTERMSIG(status)).c_str());
+					break;
+				}
+				else if (WIFSTOPPED(status))
+					DEBUG("Guarded process stopped.");
+				else if (WIFCONTINUED(status))
+					DEBUG("Guarded process continued.");
+			}
+		}
+#else
+		return handler();
+#endif
+	}
+
+	std::string sig2str(int sig)
+	{
+		static const char *sval[32] = {
+			"SIGHUP",	// 1-5
+			"SIGINT",
+			"SIGQUIT",
+			"SIGILL",
+			0,
+			"SIGABRT",	// 6-10
+			0,
+			"SIGFPE",
+			"SIGKILL",
+			0,
+			"SIGSEGV",	// 11-15
+			0,
+			"SIGPIPE",
+			"SIGALRM",
+			"SIGUSR1",
+			"SIGUSR2",
+			0
+		};
+
+		char buf[64];
+		snprintf(buf, sizeof(buf), "%s (%d)", sval[sig - 1], sig);
+		return buf;
+	}
+
+	int _run()
+	{
+		::signal(SIGHUP, &reload_handler);
+		::signal(SIGTERM, &terminate_handler);
+		::signal(SIGPIPE, SIG_IGN);
+
+		server_.run();
+
+		return 0;
 	}
 
 private:
@@ -72,6 +170,7 @@ private:
 		{
 			{ "no-fork", no_argument, &nofork_, 1 },
 			{ "fork", no_argument, &nofork_, 0 },
+			{ "guard", no_argument, &doguard_, 'G' },
 			//.
 			{ "version", no_argument, 0, 'v' },
 			{ "copyright", no_argument, 0, 'y' },
@@ -93,7 +192,7 @@ private:
 		for (;;)
 		{
 			int long_index = 0;
-			switch (getopt_long(argc, argv, "vyc:hX", long_options, &long_index))
+			switch (getopt_long(argc, argv, "vyc:hXG", long_options, &long_index))
 			{
 				case 'c':
 					configfile_ = optarg;
@@ -125,6 +224,9 @@ private:
 				case 'X':
 					nofork_ = true;
 					break;
+				case 'G':
+					doguard_ = true;
+					break;
 				case 0: // long option with (val!=NULL && flag=0)
 					break;
 				case -1: // EOF - everything parsed.
@@ -148,7 +250,7 @@ private:
 	{
 		if (instance_)
 		{
-			LOG(instance_->server_, x0::severity::info, "SIGHUP received. Reloading configuration.");
+			instance_->server_.log(x0::severity::info, "SIGHUP received. Reloading configuration.");
 
 			try
 			{
@@ -156,7 +258,7 @@ private:
 			}
 			catch (std::exception& e)
 			{
-				LOG(instance_->server_, x0::severity::error, "uncaught exception in reload handler: %s", e.what());
+				instance_->server_.log(x0::severity::error, "uncaught exception in reload handler: %s", e.what());
 			}
 		}
 	}
@@ -165,7 +267,7 @@ private:
 	{
 		if (instance_)
 		{
-			LOG(instance_->server_, x0::severity::info, "SIGTERM received. Shutting down.");
+			instance_->server_.log(x0::severity::info, "SIGTERM received. Shutting down.");
 
 			try
 			{
@@ -173,7 +275,7 @@ private:
 			}
 			catch (std::exception& e)
 			{
-				LOG(instance_->server_, x0::severity::error, "uncaught exception in terminate handler: %s", e.what());
+				instance_->server_.log(x0::severity::error, "uncaught exception in terminate handler: %s", e.what());
 			}
 		}
 	}
@@ -181,6 +283,7 @@ private:
 private:
 	std::string configfile_;
 	int nofork_;
+	int doguard_;
 	x0::server server_;
 	static x0d *instance_;
 };
