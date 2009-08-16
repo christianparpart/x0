75f07cce9fea46fb8824714a7d13c1706f206c44
Christian Parpart <trapni@gentoo.org>
2009-08-16 12:27:43 +0200

    improved configuration flexibility of vhost_basic plugin
    * allow explicit definition of default virtual host
    * added support for servername aliases

--- src/x0/mod_vhost_basic.cpp
+++ src/x0/mod_vhost_basic.cpp
@@ -9,6 +9,7 @@
 #include <x0/request.hpp>
 #include <x0/response.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/signal.hpp>
 #include <boost/bind.hpp>
 
 #include <sys/types.h>
@@ -18,6 +19,25 @@
 /**
  * \ingroup modules
  * \brief provides a basic config-file based virtual hosting facility.
+ *
+ * <pre>
+ * -- example configuration
+ *
+ * BindAddress = '0::0';             -- default bind address
+ * Listen = 80;                      -- default listening port
+ * DefaultHost = 'www.example.com';  -- default vhost to choose when we receive an unknown Host-request-header.
+ *
+ * Hosts = {
+ *     ['www.example.com'] = {
+ *         ServerAliases = { 'www.example.net', 'example.com', 'example.net' };
+ *         DocumentRoot = '/var/www/example.com/htdocs';
+ *     };
+ *     ['localhost:8080'] = {
+ *         DocumentRoot = '/var/www/example.com/htdocs';
+ *         BindAddress = 'localhost';
+ *     };
+ * };
+ * </pre>
  */
 class vhost_basic_plugin :
 	public x0::plugin
@@ -28,6 +48,12 @@ private:
 	struct vhost_config
 	{
 		std::string docroot;
+	};
+
+	struct server_config
+	{
+		std::string default_host;
+		std::map<std::string, vhost_config *> mappings;
 	};
 
 public:
@@ -45,46 +71,115 @@ public:
 	virtual void configure()
 	{
 		auto hostnames = server_.config()["Hosts"].keys<std::string>();
+		if (hostnames.empty())
+			return;
+
+		std::string default_bind("0::0");
+		server_.config().load("BindAddress", default_bind);
+
+		int default_port(80);
+		server_.config().load("Listen", default_port);
+
+		server_config& srvcfg = server_.create_context<server_config>(this);
+
+		server_.config().load("DefaultHost", srvcfg.default_host);
+		if (srvcfg.default_host.find(":") == std::string::npos && default_port != 80)
+		{
+			srvcfg.default_host = srvcfg.default_host + ":" + boost::lexical_cast<std::string>(default_port);
+		}
 
 		for (auto i = hostnames.begin(), e = hostnames.end(); i != e; ++i)
 		{
 			std::string hostname(*i);
-			vhost_config& cfg = server_.create_context<vhost_config>(this, hostname);
-			cfg.docroot = server_.config()["Hosts"][hostname]["DocumentRoot"].as<std::string>();
+
+			auto aliases = server_.config()["Hosts"][hostname]["ServerAliases"].as<std::vector<std::string>>();
+
+			std::string docroot = server_.config()["Hosts"][hostname]["DocumentRoot"].as<std::string>();
+
+			std::string bind = server_.config()["Hosts"][hostname]["BindAddress"].as<std::string>();
+			if (bind.empty())
+				bind = default_bind;
 
 			int port = extract_port(hostname);
+			if (!port)
+			{
+				port = default_port;
 
-			std::string bind_address = server_.config()["Hosts"][hostname]["BindAddress"].as<std::string>();
-			if (bind_address.empty())
-				bind_address = "0::0";
+				if (port != 80)
+				{
+					hostname = hostname + ":" + boost::lexical_cast<std::string>(port);
+				}
+			}
 
-			server_.log(x0::severity::debug, "port=%d: hostname[%s]: docroot[%s]", port, hostname.c_str(), cfg.docroot.c_str());
+//			server_.log(x0::severity::debug, "port=%d: hostname[%s]: docroot[%s], alias-count=%d",
+//				port, hostname.c_str(), docroot.c_str(), aliases.size());
 
-			server_.setup_listener(port, bind_address);
+			vhost_config& cfg = server_.create_context<vhost_config>(this, hostname);
+			cfg.docroot = docroot;
+
+			for (auto k = aliases.begin(), m = aliases.end(); k != m; ++k)
+			{
+				std::string name(*k);
+				if (name.find(":") == std::string::npos && port != 80)
+				{
+					name = name + ":" + boost::lexical_cast<std::string>(port);
+				}
+
+				auto f = srvcfg.mappings.find(name);
+				if (f == srvcfg.mappings.end())
+				{
+					srvcfg.mappings[name] = &cfg;
+				}
+				else
+				{
+					server_.log(x0::severity::warn, "Server alias '%s' already in use.", name.c_str());
+				}
+			}
+
+			server_.setup_listener(port, bind);
 		}
 	}
 
+private:
 	int extract_port(const std::string& hostname)
 	{
-		std::size_t n = hostname.find(":");
+		static std::string delim(":");
+		std::size_t n = hostname.find(delim);
+
 		if (n == std::string::npos)
-			return 80; // default
+			return 0;
 
 		return boost::lexical_cast<int>(hostname.substr(n + 1));
 	}
 
-private:
 	void resolve_document_root(x0::request& in)
 	{
 		try
 		{
-			std::string hostname(in.header("Host"));
-			vhost_config& cfg = server_.context<vhost_config>(this, hostname);
-			in.document_root = cfg.docroot;
+			static std::string hostkey("Host");
+			std::string name(in.header(hostkey));
+
+			server_config& srvcfg = server_.context<server_config>(this);
+			auto i = srvcfg.mappings.find(name);
+			if (i != srvcfg.mappings.end())
+			{
+				in.document_root = i->second->docroot;
+			}
+			else
+			{
+				in.document_root = server_.context<vhost_config>(this, name).docroot;
+			}
 		}
 		catch (const x0::host_not_found&)
 		{
-			// eat up
+			try
+			{
+				// resolve to default host's document root
+				std::string default_host(server_.context<server_config>(this).default_host);
+				in.document_root = server_.context<vhost_config>(this, default_host).docroot;
+				// XXX we could instead auto-redirect them.
+			}
+			catch (...) {}
 		}
 	}
 };
