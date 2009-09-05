a5efdc0d281919fc617803618ed228aaf4317a49
Christian Parpart <trapni@gentoo.org>
2009-09-05 07:35:30 +0200

    preliminary support for TCP_DEFER_ACCEPT and TCP_QUICKACK.
    however, this should be generalized, i mean, it's not even configurable
    yet. and maybe make it more portable, too.

--- src/x0/listener.cpp
+++ src/x0/listener.cpp
@@ -12,6 +12,8 @@
 #include <boost/bind.hpp>
 #include <boost/lexical_cast.hpp>
 
+#include <netinet/tcp.h> // TCP_QUICKACK, TCP_DEFER_ACCEPT
+
 namespace x0 {
 
 listener::listener(server& srv)
@@ -48,11 +50,23 @@ void listener::start()
 	acceptor_.set_option(asio::ip::tcp::acceptor::keep_alive(true));
 
 #if 0
+	acceptor_.set_option(asio::ip::tcp::acceptor::quick_ack(false));
+#elif defined(TCP_QUICKACK)
+	{
+		int on = 0;
+		if (setsockopt(acceptor_.native(), IPPROTO_TCP, TCP_QUICKACK, &on, sizeof(on)) < 0)
+			throw std::runtime_error(strerror(errno));
+	}
+#endif
+
+#if 0
 	acceptor_.set_option(asio::ip::tcp::acceptor::defer_accept(true));
 #elif defined(TCP_DEFER_ACCEPT)
-	int on = 1;
-	if (setsockopt(acceptor_.native(), SOL_TCP, TCP_DEFER_ACCEPT, &on, sizeof(on)) < 0)
-		throw std::runtime_error(strerror(errno));
+	{
+		int on = 1;
+		if (setsockopt(acceptor_.native(), SOL_TCP, TCP_DEFER_ACCEPT, &on, sizeof(on)) < 0)
+			throw std::runtime_error(strerror(errno));
+	}
 #endif
 
 	acceptor_.bind(endpoint);
