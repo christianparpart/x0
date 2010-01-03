5ced1763ed32eacc5160226c5345834804e9a562
Christian Parpart <trapni@gentoo.org>
2010-01-03 17:53:17 +0100

    some more docs

--- src/x0/io/sink.hpp
+++ src/x0/io/sink.hpp
@@ -5,11 +5,28 @@
 
 namespace x0 {
 
+/** chunk consumer.
+ *
+ * A sink is a chunk consumer, e.g. by saving chunks sequentially
+ * into a local file.
+ *
+ * \see file_sink, buffer_sink, asio_sink, source
+ */
 class sink
 {
 public:
 	virtual ~sink() {}
 
+	/** pushes given \p data into this sink.
+	 *
+	 * \param data the data to push into this sink
+	 *
+	 * \return a buffer view representing the data that could not be written
+	 * 		into the sink, e.g., because the write would block, 
+	 * 		or an error occurred.
+	 * 		If the buffer view is empty, all data could be written
+	 * 		successfully.
+	 */
 	virtual buffer::view push(const buffer::view& data) = 0;
 };
 
