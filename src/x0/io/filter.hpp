5454188961db5845274d32428e55650c34bfc263
Christian Parpart <trapni@gentoo.org>
2010-01-04 10:01:54 +0100

    filter: implemented filter.once(source, sink) and filter.all(source, sink), and added some more docs

--- src/x0/io/filter.hpp
+++ src/x0/io/filter.hpp
@@ -4,6 +4,7 @@
 #include <x0/buffer.hpp>
 #include <x0/io/source.hpp>
 #include <x0/io/sink.hpp>
+#include <memory>
 
 namespace x0 {
 
@@ -18,54 +19,68 @@ namespace x0 {
 class filter
 {
 public:
+	/** processes given input data through this filter. */
 	virtual buffer process(const buffer::view& input) = 0;
 
+public:
 	buffer operator()(const buffer::view& input)
 	{
 		return process(input);
 	}
 
-#if 0
-	//! pumps source through this filter, storing the result into the sink
-	void once(source& source, sink& sink)
+	/** pumps a single source chunk through this filter, storing the result into the sink.
+	 *
+	 * \param src the source to pull from.
+	 * \param snk the sink to push the filtered data into.
+	 *
+	 * Think of a data-flow directed graph like: source -> filter -> sink.
+	 */
+	virtual bool once(source& src, sink& snk)
 	{
-		buffer b;
-		if (buffer::view chunk = source.pull(b))
-			sink(process(chunk));
+		buffer sb;
+		buffer::view chunk = src.pull(sb);
+		if (!chunk)
+			return false;
+
+		buffer pb = process(chunk);
+		if (chunk = snk.push(pb))
+			while (chunk = snk.push(chunk))
+				;
+
+		return true;
 	}
 
-	void all(source& source, sink& sink)
+	/** pumps whole source through this filter into given sink.
+	 *
+	 * \param src the source to pull from.
+	 * \param snk the sink to push the filtered data into.
+	 *
+	 * Think of a data-flow directed graph like: source -> filter -> sink.
+	 */
+	virtual void all(source& src, sink& snk)
 	{
-		buffer b;
-		while (buffer::view c = source.pull(b))
-			sink.push(process(c));
+		buffer result;
+		while (buffer::view chunk = src.pull(result))
+		{
+			buffer p(process(chunk));
+
+			if (buffer::view v = snk.push(p))
+				while (v = snk.push(v))
+					;
+
+			p = process(buffer::view());
+			if (!p.empty())
+			//if (p = process(buffer::view()))
+				if (buffer::view v = snk.push(p))
+					while (v = snk.push(v))
+						;
+
+//			result.clear();
+		}
 	}
-#endif
 };
 
-class chained_filter :
-	public filter
-{
-private:
-	filter left_;
-	filter right_;
-
-public:
-	chained_filter(const filter& left, const filter& right) :
-		left_(left), right_(right)
-	{
-	}
-#if 0
-	virtual buffer process(const buffer& data)
-	{
-		return right_(left_(data));
-	}
-#endif
-};
-
-chained_filter chain(const source&, const filter&);
-chained_filter chain(const filter&, const filter&);
-chained_filter chain(const filter&, const sink&);
+typedef std::shared_ptr<filter> filter_ptr;
 
 } // namespace x0
 
