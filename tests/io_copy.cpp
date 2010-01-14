c33f471572e3f524a60cbf9ab4b2b3c89db84dca
Christian Parpart <trapni@gentoo.org>
2010-01-14 15:42:41 +0100

    io_copy (I/O filtering) test tool improved

--- tests/io_copy.cpp
+++ tests/io_copy.cpp
@@ -10,42 +10,68 @@
 #include <x0/pump.hpp>
 #include <iostream>
 #include <memory>
+#include <getopt.h>
 
-int main(int argc, const char *argv[])
+int main(int argc, char *argv[])
 {
-	if (argc != 3 && argc != 4)
+	struct option options[] = {
+		{ "input", required_argument, 0, 'i' },
+		{ "output", required_argument, 0, 'o' },
+		{ "gzip", no_argument, 0, 'c' },
+		{ "uppercase", no_argument, 0, 'U' },
+		{ "help", no_argument, 0, 'h' },
+		{ 0, 0, 0, 0 }
+	};
+	std::string ifname("-");
+	std::string ofname("-");
+	x0::chain_filter cf;
+
+	for (bool done = false; !done; )
 	{
-		std::cerr << "usage: " << argv[0] << " INPUT OUTPUT [-u]" << std::endl;
-		std::cerr << "  where INPUT and OUTPUT can be '-' to be interpreted as stdin/stdout respectively." << std::endl;
-		return 1;
+		int index = 0;
+		int rv = (getopt_long(argc, argv, "i:o:hUc", options, &index));
+		switch (rv)
+		{
+			case 'i':
+				ifname = optarg;
+				break;
+			case 'o':
+				ofname = optarg;
+				break;
+			case 'U':
+				cf.push_back(x0::filter_ptr(new x0::uppercase_filter()));
+				break;
+			case 'c':
+				cf.push_back(x0::filter_ptr(new x0::compress_filter()));
+				break;
+			case 'h':
+				std::cerr
+					<< "usage: " << argv[0] << " INPUT OUTPUT [-u]" << std::endl
+					<< "  where INPUT and OUTPUT can be '-' to be interpreted as stdin/stdout respectively." << std::endl;
+				return 0;
+			case 0:
+				break;
+			case -1:
+				done = true;
+				break;
+			default:
+				std::cerr << "syntax error: "
+						  << "(" << rv << ")" << std::endl;
+				return 1;
+		}
 	}
 
-	std::string ifn(argv[1]);
-	std::shared_ptr<x0::source> input(ifn == "-" 
+	std::shared_ptr<x0::source> input(ifname == "-" 
 		? new x0::fd_source(STDIN_FILENO)
-		: new x0::file_source(ifn)
+		: new x0::file_source(ifname)
 	);
 
-	std::string ofn(argv[2]);
-	std::shared_ptr<x0::sink> output(ofn == "-"
+	std::shared_ptr<x0::sink> output(ofname == "-"
 		? new x0::fd_sink(STDOUT_FILENO)
-		: new x0::file_sink(ofn)
+		: new x0::file_sink(ofname)
 	);
 
-	if (argc >= 4)
-	{
-		x0::chain_filter cf;
-		if (std::string(argv[3]) == "-u")
-			cf.push_back(x0::filter_ptr(new x0::uppercase_filter()));
-		else if (std::string(argv[3]) == "-d")
-			cf.push_back(x0::filter_ptr(new x0::compress_filter()));
-
-		pump(*input, *output, cf);
-	}
-	else
-	{
-		pump(*input, *output);
-	}
+	pump(*input, *output, cf);
 
 	return 0;
 }
