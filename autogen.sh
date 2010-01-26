afa77d1bcb5ea456b764d5a7cf3c2d5143a51043
Christian Parpart <trapni@gentoo.org>
2010-01-26 05:45:08 +0100

    cmake fix

--- autogen.sh
+++ autogen.sh
@@ -4,5 +4,5 @@ if [ "$1" == "clean" ]; then
 	find . -name 'CMakeCache.txt' -print | xargs rm -vrf &>/dev/null
 	find . -name 'CMakeFiles*' -print | xargs rm -vrf &>/dev/null
 else
-	cmake . -DASIO_INCLUDEDIR="$(dirname $(pwd))/asio"
+	cmake . -DASIO_INCLUDEDIR="$(dirname $(pwd))/asio/include"
 fi
