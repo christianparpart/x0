d6095b1d1092ee1a53dac0346a181fd1b41c81fd
Christian Parpart <trapni@gentoo.org>
2010-04-16 02:27:16 +0200

    autogen.sh: update (well, currently used by me only anyways)

--- autogen.sh
+++ autogen.sh
@@ -8,8 +8,9 @@ else
 	cmake . \
 		-DASIO_INCLUDEDIR="$(dirname $(pwd))/asio/include" \
 		-DCMAKE_CXX_FLAGS_DEBUG="-O0 -ggdb3" \
-		-DCMAKE_INSTALL_PREFIX="/usr" \
-		-DSYSCONFDIR="/etc/x0" \
-		-DSTATEDIR="/var/run/x0" \
+		-DCMAKE_INSTALL_PREFIX="/opt/sandbox" \
+		-DSYSCONFDIR="/opt/sandbox/etc" \
+		-DINCLUDEDIR="/opt/sandbox/include" \
+		-DLIBDIR="/opt/sandbox/lib64" \
 		-DCMAKE_BUILD_TYPE="debug"
 fi
