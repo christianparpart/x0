f7cf91fc36aee575602bbcb351b4aa7a449c6ff2
Christian Parpart <trapni@gentoo.org>
2010-06-08 19:50:12 +0200

    [plugins] indexfile: split into header and implementation file. (to be referenced from host programs, e.g. x0d)

--- src/plugins/indexfile.cpp
+++ src/plugins/indexfile.cpp
@@ -1,15 +1,17 @@
-/* <x0/mod_indexfile.cpp>
+/* <x0/plugins/indexfile.cpp>
  *
  * This file is part of the x0 web server project and is released under LGPL-3.
  *
  * (c) 2009 Chrisitan Parpart <trapni@gentoo.org>
  */
 
+#include "indexfile.h"
 #include <x0/http/HttpPlugin.h>
 #include <x0/http/HttpServer.h>
 #include <x0/http/HttpRequest.h>
 #include <x0/http/HttpResponse.h>
 #include <x0/http/HttpHeader.h>
+#include <x0/Scope.h>
 #include <x0/strutils.h>
 #include <x0/Types.h>
 #include <cstring>
@@ -18,92 +20,82 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-/**
- * \ingroup plugins
- * \brief implements automatic index file resolving, if mapped request path is a path.
- */
-class indexfile_plugin :
-	public x0::HttpPlugin
+void indexfile_plugin::context::merge(const x0::ScopeValue *value)
 {
-private:
-	x0::HttpServer::request_parse_hook::connection c;
-
-	struct context : public x0::ScopeValue
+	if (auto cx = dynamic_cast<const context *>(value))
 	{
-		std::vector<std::string> index_files;
+		if (index_files.empty())
+			index_files = cx->index_files;
+	}
+}
 
-		virtual void merge(const x0::ScopeValue *value)
+indexfile_plugin::indexfile_plugin(x0::HttpServer& srv, const std::string& name) :
+	x0::HttpPlugin(srv, name)
+{
+	// to connect to resolved_entity at slot-group `1`, so, that all other transforms have taken place already,
+	// that is, e.g. "userdir".
+	// XXX a better implementation of this dependency-issue surely is, to introduce
+	// another signal that would order the event sequence for us, but i'm not yet that clear about how
+	// to name this in a clean and reasonable way.
+	using namespace std::placeholders;
+	c = server_.resolve_entity.connect(/*FIXME 1, */ std::bind(&indexfile_plugin::indexfile, this, _1));
+
+	server_.declareCVar("IndexFiles", x0::HttpContext::server | x0::HttpContext::host, std::bind(&indexfile_plugin::setup_indexfiles, this, _1, _2));
+}
+
+indexfile_plugin::~indexfile_plugin()
+{
+	server_.resolve_entity.disconnect(c);
+	server_.release(this);
+}
+
+std::vector<std::string> *indexfile_plugin::indexFiles(const x0::Scope& scope) const
+{
+	if (context *ctx = scope.get<context>(this))
+		return &ctx->index_files;
+
+	return 0;
+}
+
+void indexfile_plugin::setIndexFiles(x0::Scope& scope, const std::vector<std::string>& indexFiles)
+{
+	scope.acquire<context>(this)->index_files = indexFiles;
+}
+
+bool indexfile_plugin::setup_indexfiles(const x0::SettingsValue& cvar, x0::Scope& s)
+{
+	return cvar.load(s.acquire<context>(this)->index_files);
+}
+
+void indexfile_plugin::indexfile(x0::HttpRequest *in)
+{
+	if (!in->fileinfo->is_directory())
+		return;
+
+	auto files = indexFiles(server_.host(in->hostid()));
+	if (!files)
+		return;
+
+	std::string path(in->fileinfo->filename());
+
+	for (auto i = files->begin(), e = files->end(); i != e; ++i)
+	{
+		std::string ipath;
+		ipath.reserve(path.length() + 1 + i->length());
+		ipath += path;
+		if (path[path.size() - 1] != '/')
+			ipath += "/";
+		ipath += *i;
+
+		if (x0::FileInfoPtr fi = in->connection.server().fileinfo(ipath))
 		{
-			if (auto cx = dynamic_cast<const context *>(value))
+			if (fi->is_regular())
 			{
-				if (index_files.empty())
-					index_files = cx->index_files;
-			}
-		}
-	};
-
-public:
-	indexfile_plugin(x0::HttpServer& srv, const std::string& name) :
-		x0::HttpPlugin(srv, name)
-	{
-		// to connect to resolved_entity at slot-group `1`, so, that all other transforms have taken place already,
-		// that is, e.g. "userdir".
-		// XXX a better implementation of this dependency-issue surely is, to introduce
-		// another signal that would order the event sequence for us, but i'm not yet that clear about how
-		// to name this in a clean and reasonable way.
-		using namespace std::placeholders;
-		c = server_.resolve_entity.connect(/*FIXME 1, */ std::bind(&indexfile_plugin::indexfile, this, _1));
-
-		server_.declareCVar("IndexFiles", x0::HttpContext::server | x0::HttpContext::host, std::bind(&indexfile_plugin::setup_indexfiles, this, _1, _2));
-	}
-
-	~indexfile_plugin()
-	{
-		server_.resolve_entity.disconnect(c);
-		server_.release(this);
-	}
-
-	void setup_indexfiles_srv(const x0::SettingsValue& cvar)
-	{
-		cvar.load(server_.acquire<context>(this)->index_files);
-	}
-
-	bool setup_indexfiles(const x0::SettingsValue& cvar, x0::Scope& s)
-	{
-		return cvar.load(s.acquire<context>(this)->index_files);
-	}
-
-private:
-	void indexfile(x0::HttpRequest *in)
-	{
-		if (!in->fileinfo->is_directory())
-			return;
-
-		context *ctx = server_.host(in->hostid()).get<context>(this);
-		if (!ctx)
-			return;
-
-		std::string path(in->fileinfo->filename());
-
-		for (auto i = ctx->index_files.begin(), e = ctx->index_files.end(); i != e; ++i)
-		{
-			std::string ipath;
-			ipath.reserve(path.length() + 1 + i->length());
-			ipath += path;
-			if (path[path.size() - 1] != '/')
-				ipath += "/";
-			ipath += *i;
-
-			if (x0::FileInfoPtr fi = in->connection.server().fileinfo(ipath))
-			{
-				if (fi->is_regular())
-				{
-					in->fileinfo = fi;
-					break;
-				}
+				in->fileinfo = fi;
+				break;
 			}
 		}
 	}
-};
+}
 
 X0_EXPORT_PLUGIN(indexfile);
---
+++ src/plugins/indexfile.h
@@ -0,0 +1,45 @@
+/* <x0/plugins/indexfile.h>
+ *
+ * This file is part of the x0 web server project and is released under LGPL-3.
+ *
+ * (c) 2009-2010 Chrisitan Parpart <trapni@gentoo.org>
+ */
+
+#ifndef sw_x0_plugins_indexfile_h
+#define sw_x0_plugins_indexfile_h (1)
+
+#include <x0/http/HttpPlugin.h>
+#include <x0/http/HttpServer.h>
+
+/**
+ * \ingroup plugins
+ * \brief implements automatic index file resolving, if mapped request path is a path.
+ */
+class indexfile_plugin :
+	public x0::HttpPlugin
+{
+public:
+	indexfile_plugin(x0::HttpServer& srv, const std::string& name);
+	~indexfile_plugin();
+
+	bool defaultEnabled() const;
+	void setDefaultEnabled(bool value);
+
+	std::vector<std::string> *indexFiles(const x0::Scope& scope) const;
+	void setIndexFiles(x0::Scope& scope, const std::vector<std::string>& indexFiles);
+
+private:
+	x0::HttpServer::request_parse_hook::connection c;
+
+	struct context : public x0::ScopeValue
+	{
+		std::vector<std::string> index_files;
+
+		virtual void merge(const x0::ScopeValue *value);
+	};
+
+	bool setup_indexfiles(const x0::SettingsValue& cvar, x0::Scope& s);
+	void indexfile(x0::HttpRequest *in);
+};
+
+#endif
