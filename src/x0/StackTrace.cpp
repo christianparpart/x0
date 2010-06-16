e203c5e4b91a36151267b924f7da8b0c708671d3
Christian Parpart <trapni@gentoo.org>
2010-06-16 23:45:26 +0200

    [core] StackTrace: implementation improved.

--- src/x0/StackTrace.cpp
+++ src/x0/StackTrace.cpp
@@ -1,60 +1,108 @@
 #include <x0/StackTrace.h>
 #include <boost/lexical_cast.hpp>
 #include <execinfo.h>
+#include <cxxabi.h>
+#include <dlfcn.h>
+#include <strings.h>
 
 namespace x0 {
 
 StackTrace::StackTrace(int numSkipFrames, int numMaxFrames)
 {
-	skip_ = numSkipFrames;
-	symbols_ = NULL;
-	addresses_ = new void *[numSkipFrames + numMaxFrames];
-	count_ = backtrace(addresses_, numSkipFrames + numMaxFrames);
+	skip_ = 1 + numSkipFrames;
+	//buffer_.resize(numSkipFrames + numMaxFrames);
+	addresses_ = new void *[skip_ + numMaxFrames];
+	count_ = backtrace(addresses_, skip_ + numMaxFrames);
 }
 
 StackTrace::~StackTrace()
 {
 	delete[] addresses_;
-	free(symbols_);
 }
 
-void StackTrace::generate()
+void StackTrace::generate(bool verbose)
 {
-	if (!symbols_)
+	if (!symbols_.empty())
+		return;
+
+	auto stripLeftOf = [](const char *value, char ch) -> const char * {
+		const char *p = value;
+
+		for (auto i = value; *i; ++i)
+			if (*i == ch)
+				p = i;
+
+		return p != value ? p + 1 : p;
+	};
+
+	auto demangleSymbol = [](const char *symbolName, Buffer& result) {
+		if (!symbolName || !*symbolName)
+			result.push_back("<invalid symbol>");
+		else
+		{
+			char *rv = 0;
+			int status = 0;
+			std::size_t len = 1024;
+
+			try { rv = abi::__cxa_demangle(symbolName, result.end(), &len, &status); }
+			catch (...) {}
+
+			if (status < 0)
+				result.push_back(symbolName);
+			else
+				result.resize(result.size() + strlen(rv));
+		}
+	};
+
+	for (int i = 0; i < count_; ++i)
 	{
-		// TODO: demangle C++-API when possible
-		symbols_ = backtrace_symbols(addresses_ + skip_, count_);
+		void *address = addresses_[skip_ + i];
+
+		buffer_.push_back('[');
+		buffer_.push_back(i);
+		buffer_.push_back("] ");
+
+		std::size_t begin = buffer_.size();
+		buffer_.reserve(buffer_.size() + 512);
+
+		Dl_info info;
+		if (!dladdr(address, &info))
+			buffer_.push_back("<unresolved symbol>");
+		else
+		{
+			demangleSymbol(info.dli_sname, buffer_);
+			if (verbose)
+			{
+				buffer_.push_back(" in ");
+				buffer_.push_back(stripLeftOf(info.dli_fname, '/'));
+			}
+		}
+
+		std::size_t count = buffer_.size() - begin;
+		symbols_.push_back(buffer_.ref(begin, count));
+
+		buffer_.push_back("\n");
 	}
 }
 
+/** retrieves the number of frames captured. */
 int StackTrace::length() const
 {
 	return count_;
 }
 
-const char *StackTrace::at(int index) const
+/** retrieves the frame information of a frame at given index. */
+const BufferRef& StackTrace::at(int index) const
 {
 	return symbols_[index];
 }
 
+/** retrieves full stack trace information (of all captured frames). */
 const char *StackTrace::c_str() const
 {
-	const_cast<StackTrace *>(this)->generate();
+	const_cast<StackTrace *>(this)->generate(true);
 
-	std::string buf;
-	for (int i = 0; i < count_; ++i)
-	{
-		buf += " [";
-		buf += boost::lexical_cast<std::string>(i);
-		buf += "] ";
-		buf += at(i);
-		buf += "\n";
-	}
-
-	const_cast<StackTrace *>(this)->buf_ = buf;
-
-	return buf_.c_str();
+	return const_cast<StackTrace *>(this)->buffer_.c_str();
 }
-
 
 } // namespace x0
--- src/x0/StackTrace.h
+++ src/x0/StackTrace.h
@@ -2,6 +2,9 @@
 #define sw_x0_StackTrace_h 1
 
 #include <x0/Api.h>
+#include <x0/Buffer.h>
+#include <x0/BufferRef.h>
+#include <vector>
 #include <string>
 
 namespace x0 {
@@ -9,20 +12,20 @@ namespace x0 {
 class X0_API StackTrace
 {
 private:
+	Buffer buffer_;
 	void **addresses_;
-	char **symbols_;
+	std::vector<BufferRef> symbols_;
 	int skip_;
 	int count_;
-	std::string buf_;
 
 public:
 	explicit StackTrace(int numSkipFrames = 0, int numMaxFrames = 32);
 	~StackTrace();
 
-	void generate();
+	void generate(bool verbose);
 
 	int length() const;
-	const char *at(int index) const;
+	const BufferRef& at(int index) const;
 
 	const char *c_str() const;
 };
