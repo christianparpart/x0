610c1468a814829fec06fa49f0df9528e97ee4db
Christian Parpart <trapni@gentoo.org>
2010-07-21 13:40:09 +0200

    autogen.sh: tell me why this doesn't show ssl.so's symbols (fvisibility=hidden?)

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ if [ "$1" == "clean" ]; then
 else
 	cmake . \
 		-DASIO_INCLUDEDIR="$(dirname $(pwd))/asio/include" \
-		-DCMAKE_CXX_FLAGS_DEBUG="-O0 -ggdb3" \
+		-DCMAKE_CXX_FLAGS_DEBUG="-O0 -ggdb3 -rdynamic" \
 		-DCMAKE_BUILD_TYPE="debug" \
 		-DCMAKE_INSTALL_PREFIX="/opt/sandbox"
 fi
