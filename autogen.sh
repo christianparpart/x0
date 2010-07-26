c5c2632b46eef2128898da4508d80911be8484c8
Christian Parpart <trapni@gentoo.org>
2010-07-26 02:10:20 +0200

    autogen.sh: `rm -f Makefile` on cleanup, too.

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@
 if [ "$1" == "clean" ]; then
 	find . -name 'CMakeCache.txt' -print | xargs rm -vrf &>/dev/null
 	find . -name 'CMakeFiles*' -print | xargs rm -vrf &>/dev/null
+	find . -name 'Makefile' -exec rm -f {} \;
 	rm -f cmake_install.cmake
 else
 	cmake "$(dirname $0)" \
