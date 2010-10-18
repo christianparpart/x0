4b5344ae0be0416011d8acec663bf6d781f751bd
Christian Parpart <trapni@gentoo.org>
2010-10-18 18:13:38 +0200

    miscs.

--- www/htdocs/test.sh
+++ www/htdocs/test.sh
@@ -3,4 +3,10 @@
 echo "Hello: World"
 echo "Content-Type: text/plain"
 echo ""
+echo "listing open FDs of self ($$)"
+ls -hl /proc/$$/fd/
+for i in `seq 8 -1 1`; do
+	echo "poke $i"
+	sleep 1;
+done
 echo "Hello, World"
