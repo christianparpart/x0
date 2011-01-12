5cb076e0d546a8eb28a58076ec8e5888ef9a74b1
Christian Parpart <trapni@gentoo.org>
2011-01-12 16:51:37 +0100

    PHP-version of a upload-test script.
    used to test proper request body passing from web client through x0d to
    the CGI or FastCGI backend.

---
+++ www/htdocs/upload.php
@@ -0,0 +1,23 @@
+
+<form enctype="multipart/form-data" action="upload.php" method="POST">
+	Choose a file to upload: <input name="uploadedfile" type="file" /><br />
+	<input type="submit" value="Upload File" />
+</form>
+
+<?php
+if ($_FILES['uploadedfile']) { // FIXME this generates a PHP warning - but how to check else?
+	// Where the file is going to be placed 
+	$target_path = "/tmp/";
+
+	/* Add the original filename to our target path.  
+	   Result is "uploads/filename.extension" */
+	$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
+
+	if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+			echo "The file ".  basename( $_FILES['uploadedfile']['name']). 
+					" has been uploaded";
+	} else{
+			echo "There was an error uploading the file, please try again! (" . $target_path . ')';
+	}
+}
+?>
