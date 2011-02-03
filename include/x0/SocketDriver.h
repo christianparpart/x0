606406bff187ff881ceba61690397d23048b3641
Christian Parpart <trapni@gentoo.org>
2011-02-04 00:32:18 +0100

    SocketDriver: add a basic `Socket *create(IPAddress, port)` for client-socket creation.

--- include/x0/SocketDriver.h
+++ include/x0/SocketDriver.h
@@ -13,6 +13,10 @@
 #include <system_error>
 #include <unistd.h>
 
+namespace Flow {
+	class IPAddress;
+}
+
 namespace x0 {
 
 class Socket;
@@ -25,6 +29,7 @@ public:
 
 	virtual bool isSecure() const;
 	virtual Socket *create(struct ev_loop *loop, int handle, int af);
+	virtual Socket *create(struct ev_loop *loop, Flow::IPAddress *ipaddr, int port);
 	virtual void destroy(Socket *);
 };
 
--- lib/base/SocketDriver.cpp
+++ lib/base/SocketDriver.cpp
@@ -8,7 +8,10 @@
 
 #include <x0/SocketDriver.h>
 #include <x0/Socket.h>
+#include <flow/IPAddress.h>
 #include <ev++.h>
+
+#include <sys/socket.h>
 
 namespace x0 {
 
@@ -28,6 +31,45 @@ bool SocketDriver::isSecure() const
 Socket *SocketDriver::create(struct ev_loop *loop, int handle, int af)
 {
 	return new Socket(loop, handle, af);
+}
+
+Socket *SocketDriver::create(struct ev_loop *loop, Flow::IPAddress *ipaddr, int port)
+{
+	int fd = ::socket(ipaddr->family(), SOCK_STREAM, 0);
+	if (fd < 0) {
+		perror("socket");
+		return nullptr;
+	}
+
+	char buf[sizeof(sockaddr_in6)];
+	std::size_t size;
+	memset(&buf, 0, sizeof(buf));
+	switch (ipaddr->family()) {
+		case Flow::IPAddress::V4:
+			size = sizeof(sockaddr_in);
+			((sockaddr_in *)buf)->sin_port = htons(port);
+			((sockaddr_in *)buf)->sin_family = AF_INET;
+			memcpy(&((sockaddr_in *)buf)->sin_addr, ipaddr->data(), ipaddr->size());
+			break;
+		case Flow::IPAddress::V6:
+			size = sizeof(sockaddr_in6);
+			((sockaddr_in6 *)buf)->sin6_port = htons(port);
+			((sockaddr_in6 *)buf)->sin6_family = AF_INET6;
+			memcpy(&((sockaddr_in6 *)buf)->sin6_addr, ipaddr->data(), ipaddr->size());
+			break;
+		default:
+			::close(fd);
+			return nullptr;
+	}
+
+	int rv = ::connect(fd, (const sockaddr *)buf, size);
+	if (rv < 0) {
+		::close(fd);
+		perror("connect");
+		return nullptr;
+	}
+
+	return new Socket(loop, fd, ipaddr->family());
 }
 
 void SocketDriver::destroy(Socket *socket)
