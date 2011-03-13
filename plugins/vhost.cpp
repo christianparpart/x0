43de21c8c06cd1cdfbbce646e7c2a0f5a806f1c6
Christian Parpart <trapni@gentoo.org>
2011-03-13 19:55:00 +0100

    [plugins] vhost: new basic virtual host mapping plugin, making use of new Flow language feature (handler references)
    Thus, allowing to register new virtual hosts like:
    
    handler setup {
        # ...
        vhost.add 'example.com:8080' => example.com
        # ...
    }
    
    handler main {
        # ...
        vhost.map
        # ...
    }
    
    handler example.com {
        # ... it is now ensured, that this code here is only invoked when
        # host is example.com at port 8080
    }

---
+++ plugins/vhost.cpp
@@ -0,0 +1,95 @@
+/* <x0/plugins/vhost.cpp>
+ *
+ * This file is part of the x0 web server project and is released under LGPL-3.
+ * http://www.xzero.ws/
+ *
+ * (c) 2011 Christian Parpart <trapni@gentoo.org>
+ *
+ * --------------------------------------------------------------------------
+ *
+ * plugin type: hostname resolver
+ *
+ * description:
+ *     Maps the request hostname:port to a dedicated handler.
+ *
+ * setup API:
+ *     void vhost.add(FQDN => handler_ref);
+ *
+ * request processing API:
+ *     handler vhost.map();
+ */
+
+#include <x0/http/HttpPlugin.h>
+#include <x0/http/HttpServer.h>
+#include <x0/http/HttpRequest.h>
+#include <x0/http/HttpHeader.h>
+#include <x0/Types.h>
+
+#include <unordered_map>
+
+/**
+ * \ingroup plugins
+ * \brief virtual host mapping plugin
+ */
+class vhost_plugin :
+	public x0::HttpPlugin
+{
+private:
+	std::unordered_map<std::string, Flow::Value::Function> namedHosts_;
+
+public:
+	vhost_plugin(x0::HttpServer& srv, const std::string& name) :
+		x0::HttpPlugin(srv, name)
+	{
+		registerSetupFunction<vhost_plugin, &vhost_plugin::addHost>("vhost.add", Flow::Value::VOID);
+		registerHandler<vhost_plugin, &vhost_plugin::mapRequest>("vhost.map");
+	}
+
+	~vhost_plugin()
+	{
+	}
+
+private:
+	// vhost.add fqdn => proc, ...
+	void addHost(Flow::Value& result, const x0::Params& args)
+	{
+		for (auto& arg: args)
+			registerHost(arg);
+	}
+
+	void registerHost(const Flow::Value& arg)
+	{
+		if (arg.type() == Flow::Value::ARRAY) {
+			const Flow::Value* args = arg.toArray();
+			if (args[0].isVoid() || args[1].isVoid() || !args[2].isVoid())
+				return;
+
+			const Flow::Value& fqdn = args[0];
+			const Flow::Value& proc = args[1];
+
+			if (!fqdn.isString())
+				return;
+
+			if (!proc.isFunction())
+				return;
+
+			registerHost(fqdn.toString(), proc.toFunction());
+		}
+	}
+
+	void registerHost(const char *fqdn, Flow::Value::Function handler)
+	{
+		namedHosts_[fqdn] = handler;
+	}
+
+	bool mapRequest(x0::HttpRequest *r, const x0::Params& args)
+	{
+		auto i = namedHosts_.find(r->hostid());
+		if (i != namedHosts_.end())
+			return i->second(r);
+
+		return false;
+	}
+};
+
+X0_EXPORT_PLUGIN(vhost)
