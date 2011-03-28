ac938e2e9dfb39a9ca7ba68c1a6c66f630f50553
Christian Parpart <trapni@gentoo.org>
2011-03-28 21:37:45 +0200

    [base] TimeSpan: added .cpp file to TimeSpan (required for global static Zero)

---
+++ lib/base/TimeSpan.cpp
@@ -0,0 +1,15 @@
+/* <x0/TimeSpan.cpp>
+ *
+ * This file is part of the x0 web server project and is released under LGPL-3.
+ * http://www.xzero.ws/
+ *
+ * (c) 2009-2011 Christian Parpart <trapni@gentoo.org>
+ */
+
+#include <x0/TimeSpan.h>
+
+namespace x0 {
+
+const TimeSpan TimeSpan::Zero(0lu);
+
+} // namespace x0
