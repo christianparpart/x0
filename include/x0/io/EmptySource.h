d961399dd2f1753fef2e1c1f5550e4fbd06a38ce
Christian Parpart <trapni@gentoo.org>
2011-04-29 18:56:41 +0200

    [io] EmptySource: new class, representing a source sending nothing.

---
+++ include/x0/io/EmptySource.h
@@ -0,0 +1,37 @@
+/* <EmptySource.h>
+ *
+ * This file is part of the x0 web server project and is released under LGPL-3.
+ * http://www.xzero.ws/
+ *
+ * (c) 2009-2010 Christian Parpart <trapni@gentoo.org>
+ */
+
+#ifndef sw_x0_io_EmptySource_hpp
+#define sw_x0_io_EmptySource_hpp 1
+
+#include <x0/io/Source.h>
+#include <memory>
+
+namespace x0 {
+
+//! \addtogroup io
+//@{
+
+/** an empty source, sending nothing.
+ *
+ * \see Buffer, Source, Sink
+ */
+class X0_API EmptySource :
+	public Source
+{
+public:
+	virtual ssize_t sendto(Sink& sink);
+
+	virtual const char* className() const;
+};
+
+//@}
+
+} // namespace x0
+
+#endif
