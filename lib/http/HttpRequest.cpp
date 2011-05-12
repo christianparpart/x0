68d00896dca77804292a07f2eef3c96fb50c8618
Christian Parpart <trapni@gentoo.org>
2011-05-13 01:29:50 +0200

    [http] HttpRequest: changed the way hostid() is fetched. (now from pre-fetched hostname instead of grepping through the whole table again)

--- lib/http/HttpRequest.cpp
+++ lib/http/HttpRequest.cpp
@@ -106,7 +106,7 @@ BufferRef HttpRequest::requestHeader(const std::string& name) const
 std::string HttpRequest::hostid() const
 {
 	if (hostid_.empty())
-		hostid_ = x0::make_hostid(requestHeader("Host"), connection.listener().port());
+		hostid_ = x0::make_hostid(hostname, connection.listener().port());
 
 	return hostid_;
 }
