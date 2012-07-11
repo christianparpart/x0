4d7b6681eae9fa2e9ce7a4a7d78548ac49b69e7f
Christian Parpart <trapni@gentoo.org>
2012-07-11 08:33:34 +0200

    [io] FileSink: do not actually open /dev/stdout and /dev/stderr but re-use their existing file descriptors.
    This effectively fixes issues when logging to stdout/stderr from within
    a tmux session, which did not display these messages et al.
    Indeed, one might want to fix tmux, too. :-)

--- lib/io/FileSink.cpp
+++ lib/io/FileSink.cpp
@@ -15,17 +15,40 @@
 
 namespace x0 {
 
+/*! Initializes a file sink.
+ *
+ * \param filename path to the fielsystem's file.  The filename \b /dev/stdout and \b /dev/stderr
+ *                 are handled specially, not directly opening that file, but assigning
+ *                 its well known file descriptors (1 and 2) directly.
+ *                 These file descriptors, unlike any other, are not closed on destruction.
+ * \param flags    Flags, passed to the open(2) system call, e.g. O_WRONLY.
+ *
+ * If possible, the FD_CLOEXEC flag is enabled after a successful open(2), too, but
+ * if you rely on this behavior and are in a multi threaded environment with
+ * fork()'s in between, it is generally recommended to pass \b O_CLOEXEC as \a flag aswell.
+ */
+
 FileSink::FileSink(const std::string& filename, int flags)
 {
-	handle_ = open(filename.c_str(), flags, 0666);
-	if (handle_ >= 0) {
-		fcntl(handle_, F_SETFD, fcntl(handle_, F_GETFD) | FD_CLOEXEC);
+	if (filename == "/dev/stdout")
+		handle_ = 1;
+	else if (filename == "/dev/stderr")
+		handle_ = 2;
+	else {
+		handle_ = open(filename.c_str(), flags, 0666);
+		if (handle_ >= 0) {
+			fcntl(handle_, F_SETFD, fcntl(handle_, F_GETFD) | FD_CLOEXEC);
+		}
 	}
 }
 
+/*! Frees object's file-descriptor.
+ *
+ * \note As mentioned in the constructor, \b /dev/stdout and \b /dev/stderr are not closed.
+ */
 FileSink::~FileSink()
 {
-	if (handle_ >= 0) {
+	if (handle_ >= 3) {
 		::close(handle_);
 	}
 }
