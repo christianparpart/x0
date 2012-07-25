4605f53bf928ef809a785e044a546e54d05ec081
Christian Parpart <trapni@gentoo.org>
2012-07-25 12:20:47 +0200

    [plugin] director: code cleanup

--- plugins/director/DirectorPlugin.cpp
+++ plugins/director/DirectorPlugin.cpp
@@ -25,6 +25,7 @@
  *     handler director.pass(string director_name);
  */
 
+#include "DirectorPlugin.h"
 #include "Director.h"
 #include "Backend.h"
 #include "ApiRequest.h"
@@ -36,234 +37,158 @@
 
 using namespace x0;
 
-static inline std::string urldecode(const std::string& AString) { // {{{
-	Buffer sb;
+DirectorPlugin::DirectorPlugin(HttpServer& srv, const std::string& name) :
+	HttpPlugin(srv, name),
+	directors_()
+{
+	registerSetupFunction<DirectorPlugin, &DirectorPlugin::director_create>("director.create", FlowValue::VOID);
+	registerSetupFunction<DirectorPlugin, &DirectorPlugin::director_load>("director.load", FlowValue::VOID);
+	registerHandler<DirectorPlugin, &DirectorPlugin::director_pass>("director.pass");
+	registerHandler<DirectorPlugin, &DirectorPlugin::director_api>("director.api");
+}
 
-    for (std::string::size_type i = 0, e = AString.size(); i < e; ++i) {
-        if (AString[i] == '%') {
-			std::string snum(AString.substr(++i, 2));
-            ++i;
-			sb.push_back(char(std::strtol(snum.c_str(), 0, 16) & 0xFF));
-        } else if (AString[i] == '+')
-			sb.push_back(' ');
-        else
-			sb.push_back(AString[i]);
-    }
+DirectorPlugin::~DirectorPlugin()
+{
+	for (auto director: directors_)
+		delete director.second;
+}
 
-    return sb.str();
-} // }}}
+// {{{ setup_function director.load(...)
+void DirectorPlugin::director_load(const FlowParams& args, FlowValue& result)
+{
+	for (auto& arg: args) {
+		if (!arg.isArray())
+			continue;
 
-static inline std::unordered_map<std::string, std::string> parseArgs(const char *AQuery) { // {{{
-	std::unordered_map<std::string, std::string> args;
-	const char *data = AQuery;
+		const FlowArray& fa = arg.toArray();
+		if (fa.size() != 2)
+			continue;
 
-	for (const char *p = data; *p; ) {
-		unsigned len = 0;
-		const char *q = p;
+		const FlowValue& directorName = fa[0];
+		if (!directorName.isString())
+			continue;
 
-		while (*q && *q != '=' && *q != '&') {
-			++q;
-			++len;
-		}
+		const FlowValue& path = fa[1];
+		if (!path.isString())
+			continue;
 
-		if (len) {
-			std::string name(p, 0, len);
-			p += *q == '=' ? len + 1 : len;
+		server().log(Severity::debug, "director: Loading director %s from %s.",
+			directorName.toString(), path.toString());
 
-			len = 0;
-			for (q = p; *q && *q != '&'; ++q, ++len)
-				;
+		Director* director = new Director(server().nextWorker(), directorName.toString());
+		if (!director)
+			continue;
 
-			if (len) {
-				std::string value(p, 0, len);
-				p += len;
+		director->load(path.toString());
 
-				for (; *p == '&'; ++p)
-					; // consume '&' chars (usually just one)
-
-				args[urldecode(name)] = urldecode(value);
-			} else {
-				if (*p)
-					++p;
-
-				args[urldecode(name)] = "";
-			}
-		} else if (*p) // && or ?& or &=
-			++p;
+		directors_[directorName.toString()] = director;
 	}
-	return std::move(args);
 }
 // }}}
 
-static inline BufferRef extractBoundary(const BufferRef& contentType) //{{{
+// {{{ setup_function director.create(...)
+void DirectorPlugin::director_create(const FlowParams& args, FlowValue& result)
 {
-	size_t i = contentType.find("boundary=");
-	if (i == BufferRef::npos)
-		return BufferRef();
+	const FlowValue& directorId = args[0];
+	if (!directorId.isString())
+		return;
 
-	return contentType.ref(i + 9);
-} // }}}
+	Director* director = createDirector(directorId.toString());
+	if (!director)
+		return;
 
-class DirectorPlugin : // {{{
-	public HttpPlugin
+	for (auto& arg: args.shift()) {
+		if (!arg.isArray())
+			continue;
+
+		const FlowArray& fa = arg.toArray();
+		if (fa.size() != 2)
+			continue;
+
+		const FlowValue& backendName = fa[0];
+		if (!backendName.isString())
+			continue;
+
+		const FlowValue& backendUrl = fa[1];
+		if (!backendUrl.isString())
+			continue;
+
+		registerBackend(director, backendName.toString(), backendUrl.toString());
+	}
+
+	directors_[director->name()] = director;
+}
+
+Director* DirectorPlugin::createDirector(const char* id)
 {
-private:
-	std::unordered_map<std::string, Director*> directors_;
+	server().log(Severity::debug, "director: Creating director %s", id);
+	Director* director = new Director(server().nextWorker(), id);
+	return director;
+}
 
-public:
-	DirectorPlugin(HttpServer& srv, const std::string& name) :
-		HttpPlugin(srv, name)
-	{
-		registerSetupFunction<DirectorPlugin, &DirectorPlugin::director_create>("director.create", FlowValue::VOID);
-		registerSetupFunction<DirectorPlugin, &DirectorPlugin::director_load>("director.load", FlowValue::VOID);
-		registerHandler<DirectorPlugin, &DirectorPlugin::director_pass>("director.pass");
-		registerHandler<DirectorPlugin, &DirectorPlugin::director_api>("director.api");
-	}
+Backend* DirectorPlugin::registerBackend(Director* director, const char* name, const char* url)
+{
+	server().log(Severity::debug, "director: %s, backend %s: %s",
+			director->name().c_str(), name, url);
 
-	~DirectorPlugin()
-	{
-		for (auto director: directors_)
-			delete director.second;
-	}
+	return director->createBackend(name, url);
+}
+// }}}
 
-private:
-	// {{{ setup_function director.load(...)
-	void director_load(const FlowParams& args, FlowValue& result)
-	{
-		for (auto& arg: args) {
-			if (!arg.isArray())
-				continue;
+// {{{ handler director.pass(string director_id);
+bool DirectorPlugin::director_pass(HttpRequest* r, const FlowParams& args)
+{
+	Director* director = selectDirector(r, args);
+	if (!director)
+		return false;
 
-			const FlowArray& fa = arg.toArray();
-			if (fa.size() != 2)
-				continue;
+	server().log(Severity::debug, "director: passing request to %s.", director->name().c_str());
+	director->schedule(r);
+	return true;
+}
 
-			const FlowValue& directorName = fa[0];
-			if (!directorName.isString())
-				continue;
-
-			const FlowValue& path = fa[1];
-			if (!path.isString())
-				continue;
-
-			server().log(Severity::debug, "director: Loading director %s from %s.",
-				directorName.toString(), path.toString());
-
-			Director* director = new Director(server().nextWorker(), directorName.toString());
-			if (!director)
-				continue;
-
-			director->load(path.toString());
-
-			directors_[directorName.toString()] = director;
-		}
-	}
-	// }}}
-
-	// {{{ setup_function director.create(...)
-	void director_create(const FlowParams& args, FlowValue& result)
-	{
-		const FlowValue& directorId = args[0];
-		if (!directorId.isString())
-			return;
-
-		Director* director = createDirector(directorId.toString());
-		if (!director)
-			return;
-
-		for (auto& arg: args.shift()) {
-			if (!arg.isArray())
-				continue;
-
-			const FlowArray& fa = arg.toArray();
-			if (fa.size() != 2)
-				continue;
-
-			const FlowValue& backendName = fa[0];
-			if (!backendName.isString())
-				continue;
-
-			const FlowValue& backendUrl = fa[1];
-			if (!backendUrl.isString())
-				continue;
-
-			registerBackend(director, backendName.toString(), backendUrl.toString());
-		}
-
-		directors_[director->name()] = director;
-	}
-
-	Director* createDirector(const char* id)
-	{
-		server().log(Severity::debug, "director: Creating director %s", id);
-		Director* director = new Director(server().nextWorker(), id);
-		return director;
-	}
-
-	Backend* registerBackend(Director* director, const char* name, const char* url)
-	{
-		server().log(Severity::debug, "director: %s, backend %s: %s",
-				director->name().c_str(), name, url);
-
-		return director->createBackend(name, url);
-	}
-	// }}}
-
-	// {{{ handler director.pass(string director_id);
-	bool director_pass(HttpRequest* r, const FlowParams& args)
-	{
-		Director* director = selectDirector(r, args);
-		if (!director)
-			return false;
-
-		server().log(Severity::debug, "director: passing request to %s.", director->name().c_str());
-		director->schedule(r);
-		return true;
-	}
-
-	Director* selectDirector(HttpRequest* r, const FlowParams& args)
-	{
-		switch (args.size()) {
-			case 0: {
-				if (directors_.size() != 1) {
-					r->log(Severity::error, "director: No directors configured.");
-					return nullptr;
-				}
-				return directors_.begin()->second;
-			}
-			case 1: {
-				if (!args[0].isString()) {
-					r->log(Severity::error, "director: Passed director configured.");
-					return nullptr;
-				}
-				const char* directorId = args[0].toString();
-				auto i = directors_.find(directorId);
-				if (i == directors_.end()) {
-					r->log(Severity::error, "director: No director with name '%s' configured.", directorId);
-					return nullptr;
-				}
-				return i->second;
-			}
-			default: {
-				r->log(Severity::error, "director: Too many arguments passed, to director.pass().");
+Director* DirectorPlugin::selectDirector(HttpRequest* r, const FlowParams& args)
+{
+	switch (args.size()) {
+		case 0: {
+			if (directors_.size() != 1) {
+				r->log(Severity::error, "director: No directors configured.");
 				return nullptr;
 			}
+			return directors_.begin()->second;
+		}
+		case 1: {
+			if (!args[0].isString()) {
+				r->log(Severity::error, "director: Passed director configured.");
+				return nullptr;
+			}
+			const char* directorId = args[0].toString();
+			auto i = directors_.find(directorId);
+			if (i == directors_.end()) {
+				r->log(Severity::error, "director: No director with name '%s' configured.", directorId);
+				return nullptr;
+			}
+			return i->second;
+		}
+		default: {
+			r->log(Severity::error, "director: Too many arguments passed, to director.pass().");
+			return nullptr;
 		}
 	}
-	// }}}
+}
+// }}}
 
-	// {{{ handler director.api(string prefix);
-	bool director_api(HttpRequest* r, const FlowParams& args)
-	{
-		const char* prefix = args[0].toString();
-		if (!r->path.begins(prefix))
-			return false;
+// {{{ handler director.api(string prefix);
+bool DirectorPlugin::director_api(HttpRequest* r, const FlowParams& args)
+{
+	const char* prefix = args[0].toString();
+	if (!r->path.begins(prefix))
+		return false;
 
-		BufferRef path(r->path.ref(strlen(prefix)));
+	BufferRef path(r->path.ref(strlen(prefix)));
 
-		return ApiReqeust::process(&directors_, r, path);
-	}
-	// }}}
-}; // }}}
+	return ApiReqeust::process(&directors_, r, path);
+}
+// }}}
 
 X0_EXPORT_PLUGIN_CLASS(DirectorPlugin)
---
+++ plugins/director/DirectorPlugin.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <x0/http/HttpPlugin.h>
+
+#include <string>
+#include <unordered_map>
+
+namespace x0 {
+	class HttpServer;
+	class HttpRequest;
+}
+
+class Director;
+class Backend;
+
+class DirectorPlugin : // {{{
+	public x0::HttpPlugin
+{
+private:
+	std::unordered_map<std::string, Director*> directors_;
+
+public:
+	DirectorPlugin(x0::HttpServer& srv, const std::string& name);
+	~DirectorPlugin();
+
+private:
+	void director_load(const x0::FlowParams& args, x0::FlowValue& result);
+	void director_create(const x0::FlowParams& args, x0::FlowValue& result);
+
+	bool director_pass(x0::HttpRequest* r, const x0::FlowParams& args);
+	bool director_api(x0::HttpRequest* r, const x0::FlowParams& args);
+
+	Director* createDirector(const char* id);
+	Backend* registerBackend(Director* director, const char* name, const char* url);
+	Director* selectDirector(x0::HttpRequest* r, const x0::FlowParams& args);
+};
