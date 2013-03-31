190194f8a81245cd3cce437ba75e3dc651c6bf8c
Christian Parpart <trapni@gmail.com>
2013-03-31 21:49:49 +0200

    [plugins] director: better make this log message of severity debug to avoid spamming the logs ;)

--- plugins/director/Backend.cpp
+++ plugins/director/Backend.cpp
@@ -152,7 +152,7 @@ SchedulerStatus Backend::pass(x0::HttpRequest* r)
 {
 	++load_;
 
-	r->log(Severity::info, "Processing request by director '%s' backend '%s'.", manager()->name().c_str(), name().c_str());
+	r->log(Severity::debug, "Processing request by director '%s' backend '%s'.", manager()->name().c_str(), name().c_str());
 
 	if (!process(r)) {
 		setState(HealthState::Offline);
