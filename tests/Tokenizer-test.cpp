b2f6af38f1132aa4c3182ebd0fc2606f670caed7
Christian Parpart <trapni@gmail.com>
2013-04-24 10:11:24 +0200

    [tests] added basic Tokenizer test

---
+++ tests/Tokenizer-test.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include <x0/Tokenizer.h>
+#include <x0/Buffer.h>
+
+class TokenizerTest : public ::testing::Test
+{
+public:
+	typedef x0::Tokenizer<x0::BufferRef, x0::BufferRef> BufferTokenizer;
+
+	void SetUp();
+	void TearDown();
+
+	void Tokenize3();
+};
+
+void TokenizerTest::SetUp()
+{
+}
+
+void TokenizerTest::TearDown()
+{
+}
+
+TEST_F(TokenizerTest, Tokenize3)
+{
+	x0::Buffer input("/foo/bar/com");
+	BufferTokenizer st(input.ref(1), "/");
+	std::vector<x0::BufferRef> tokens = st.tokenize();
+
+	ASSERT_EQ(3, tokens.size());
+	EXPECT_EQ("foo", tokens[0]);
+	EXPECT_EQ("bar", tokens[1]);
+	EXPECT_EQ("com", tokens[2]);
+}
