889a6090fdf681aee27c19544c018372b471f92d
Christian Parpart <trapni@gmail.com>
2013-08-11 01:31:18 +0200

    better detect proper llvm-config* binary

--- autogen.sh
+++ autogen.sh
@@ -13,6 +13,12 @@ if [[ -z "$CXX" ]]; then
 	fi
 fi
 
+# find the best matching LLVM
+: ${LLVM_CONFIG_EXECUTABLE:=$(which llvm-config-3.3 2>/dev/null)}
+: ${LLVM_CONFIG_EXECUTABLE:=$(which llvm-config-3.1 2>/dev/null)}
+: ${LLVM_CONFIG_EXECUTABLE:=$(which llvm-config-3.0 2>/dev/null)}
+: ${LLVM_CONFIG_EXECUTABLE:=$(which llvm-config 2>/dev/null)}
+
 if [[ "$1" == "clean" ]]; then
 	find . \( -name 'CMakeCache.txt' -o -name 'CMakeFiles' \
 			-o -name 'Makefile' -o -name cmake_install.cmake \
@@ -26,7 +32,7 @@ else
 		-DCMAKE_CXX_FLAGS_DEBUG="-O0 -g3" \
 		-DCMAKE_BUILD_TYPE="debug" \
 		-DCMAKE_INSTALL_PREFIX="${HOME}/local" \
-		-DLLVM_CONFIG_EXECUTABLE=/usr/bin/llvm-config \
+		-DLLVM_CONFIG_EXECUTABLE="${LLVM_CONFIG_EXECUTABLE}" \
 		-DENABLE_RRD=ON \
 		-DENABLE_IMAGEABLE=ON \
 		-DENABLE_{WEBDAV,EXAMPLES}=ON
