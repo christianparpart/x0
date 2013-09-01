79b409085a7091313933fd6702e7bb03ddd586e2
Christian Parpart <trapni@gmail.com>
2013-09-01 11:22:30 +0200

    typofix in pkg-config file for XzeroBase

--- XzeroBase.pc.cmake
+++ XzeroBase.pc.cmake
@@ -5,5 +5,5 @@ Description: x0 framework (XzeroBase, base module)
 Version: @PACKAGE_VERSION@
 Requires: libpcre >= 7.0 
 # Conflicts: 
-Libs: -L@LIBDIR@ @LIBS_RPATH@ -lXzeroBase -lpthread @POSIX_RT_LIBS@ @MYSQL_LIBS@
+Libs: -L@LIBDIR@ @LIBS_RPATH@ -lXzeroBase -lpthread @POSIX_RT_LIBS@ @MYSQL_LDFLAGS@
 Cflags: -I@INCLUDEDIR@ -pthread @CFLAGS_RDYNAMIC@ @MYSQL_CFLAGS@ @CXXFLAGS@
