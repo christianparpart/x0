3a680f9bd99c7969047b0344e31e679ba8ab6272
Christian Parpart <trapni@gmail.com>
2013-10-07 21:04:33 +0200

    capi: post data example (wip)

---
+++ examples/capi-post-data.c
@@ -0,0 +1,93 @@
+#include <x0/capi.h>
+#include <getopt.h>
+#include <ev.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct buffer_s {
+	char* body_data;
+	size_t body_size;
+	x0_server_t* server;
+} request_udata_t;
+
+void process_request(x0_request_t* r, request_udata_t* udata)
+{
+	printf("process_request()\n");
+
+	char path[1024];
+	x0_request_path(r, path, sizeof(path));
+	printf("Request-Path: %s\n", path);
+
+	printf("processed payload size: %zu\n", udata->body_size);
+
+	x0_response_status_set(r, 200);
+	x0_response_header_set(r, "Content-Type", "text/plain");
+
+	static const char msg[] = "This Is Sparta!\n";
+	x0_response_write(r, msg, sizeof(msg) - 1);
+	x0_response_finish(r);
+
+	if (strcmp(path, "/quit") == 0) {
+		x0_server_stop(udata->server);
+	}
+}
+
+void body_handler(x0_request_t* r, const char* data, size_t size, void* userdata)
+{
+	printf("body_handler(size: %zu)\n", size);
+
+	request_udata_t* udata = (request_udata_t*) userdata;
+
+	if (size) {
+		udata->body_data = realloc(udata->body_data, udata->body_size + size);
+		memcpy(udata->body_data + udata->body_size, data, size);
+		udata->body_size += size;
+	} else {
+		process_request(r, udata);
+	}
+}
+
+void request_handler(x0_request_t* r, void* userdata)
+{
+	printf("request_handler()\n");
+
+	x0_server_t* server = (x0_server_t*) userdata;
+
+	request_udata_t* udata = malloc(sizeof(request_udata_t));
+	udata->body_data = NULL;
+	udata->body_size = 0;
+	udata->server = server;
+
+	x0_request_body_callback(r, &body_handler, udata);
+}
+
+int main(int argc, const char* argv[])
+{
+	const char* bind = "0.0.0.0";
+	int port = 8080;
+	x0_server_t* server;
+	struct ev_loop* loop;
+
+	loop = ev_default_loop(0);
+	server = x0_server_create(loop);
+
+	if (x0_listener_add(server, bind, port, 128) < 0) {
+		perror("x0_listener_add");
+		x0_server_destroy(server, 0);
+		return 1;
+	}
+
+	x0_setup_timeouts(server, /*read*/ 30, /*write*/ 10);
+	x0_setup_keepalive(server, /*count*/ 5, /*timeout*/ 8);
+	x0_setup_handler(server, /*callback*/ &request_handler, /*userdata*/ server);
+
+	printf("[HTTP] Listening on %s port %d\n", bind, port);
+
+	ev_run(loop, 0);
+
+	printf("[HTTP] Shutting down\n");
+
+	x0_server_destroy(server, 0);
+
+	return 0;
+}
