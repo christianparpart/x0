ee27e111ff07d0d7cf87e6a0a0567df1b28ea40d
Christian Parpart <trapni@gmail.com>
2014-01-21 02:10:56 +0100

    cleanup

--- lib/flow/vm/Runtime.cpp
+++ lib/flow/vm/Runtime.cpp
@@ -13,14 +13,12 @@ Runtime::~Runtime()
 
 NativeCallback& Runtime::registerHandler(const std::string& name)
 {
-    printf("Runtime::registerHandler(): %s\n", name.c_str());
     builtins_.push_back(new NativeCallback(this, name));
     return *builtins_[builtins_.size() - 1];
 }
 
 NativeCallback& Runtime::registerFunction(const std::string& name, FlowType returnType)
 {
-    printf("Runtime::registerFunction(): %s\n", name.c_str());
     builtins_.push_back(new NativeCallback(this, name, returnType));
     return *builtins_[builtins_.size() - 1];
 }
@@ -43,7 +41,6 @@ NativeCallback* Runtime::find(const Signature& signature)
 
 void Runtime::unregisterNative(const std::string& name)
 {
-    printf("Runtime::unregisterNative(): %s\n", name.c_str());
     for (size_t i = 0, e = builtins_.size(); i != e; ++i) {
         if (builtins_[i] && builtins_[i]->signature().name() == name) {
             delete builtins_[i];
