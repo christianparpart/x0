christianparpart/x0

commit e7a22648caf2f9551289a8115a33b91180004747

[base] Buffer: fixes (std::string&) constructor

include/x0/Buffer.h
@@ -1455,8 +1455,8 @@ inline Buffer::Buffer(const char* v) :
 inline Buffer::Buffer(const std::string& v) :
 	MutableBuffer<mutableEnsure>()
 {
-	push_back(v.c_str(), v.size() + 1);
-	resize(v.size());
+    reserve(v.size() + 1);
+    push_back(v.c_str(), v.size() + 1);
 }
 
 inline Buffer::Buffer(const Buffer& v) :

commit 706a4014abe9c4bc809b03abd844cd2d9c70048f

[base] Buffer: fixes a bug, introduced in e7a22648caf2f9551289a8115a33b91180004747

include/x0/Buffer.h
@@ -1525,7 +1525,7 @@ inline Buffer::Buffer(const std::string& v) :
 	MutableBuffer<mutableEnsure>()
 {
     reserve(v.size() + 1);
-    push_back(v.c_str(), v.size() + 1);
+    push_back(v.data(), v.size());
 }
 
 inline Buffer::Buffer(const Buffer& v) :