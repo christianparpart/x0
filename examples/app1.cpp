816a6352317841ab4d89cd9172e36cf702f5d2c1
Christian Parpart <trapni@gmail.com>
2014-05-08 10:47:12 +0200

    [examples] app1: also abort on SIGINT

--- examples/app1.cpp
+++ examples/app1.cpp
@@ -14,12 +14,14 @@ class MyServer {
 private:
     struct ev_loop* loop_;
     ev::sig sigterm_;
+    ev::sig sigint_;
     std::unique_ptr<x0::HttpServer> http_;
 
 public:
     MyServer() :
         loop_(ev_default_loop()),
         sigterm_(loop_),
+        sigint_(loop_),
         http_()
     {
     }
@@ -29,6 +31,11 @@ public:
         if (sigterm_.is_active()) {
             ev_ref(loop_);
             sigterm_.stop();
+        }
+
+        if (sigint_.is_active()) {
+            ev_ref(loop_);
+            sigint_.stop();
         }
     }
 
@@ -48,15 +55,20 @@ public:
     int run()
     {
         std::clog << "Initializing ..." << std::endl;
-        sigterm_.set<MyServer, &MyServer::terminate_handler>(this);
+
+        sigterm_.set<MyServer, &MyServer::terminateHandler>(this);
         sigterm_.start(SIGTERM);
+        ev_unref(loop_);
+
+        sigint_.set<MyServer, &MyServer::terminateHandler>(this);
+        sigint_.start(SIGINT);
         ev_unref(loop_);
 
         http_.reset(new x0::HttpServer(loop_));
         http_->requestHandler = std::bind(&MyServer::requestHandler, this, std::placeholders::_1);
         http_->setupListener("0.0.0.0", 3000);
 
-        std::clog << "Running ..." << std::endl;
+        std::clog << "Listening on http://0.0.0.0:3000 ..." << std::endl;
         int rv = http_->run();
 
         std::clog << "Quitting ..." << std::endl;
@@ -64,9 +76,9 @@ public:
     }
 
 private:
-    void terminate_handler(ev::sig& sig, int)
+    void terminateHandler(ev::sig& sig, int)
     {
-        std::clog << "Terminate signal received ..." << std::endl;
+        std::clog << "Signal (" << sig.signum << ") received. Terminating." << std::endl;
 
         ev_ref(loop_);
         sig.stop();
