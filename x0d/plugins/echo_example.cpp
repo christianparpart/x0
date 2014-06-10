d139e2118cf9e0abe5feea269d4db55815e090b8
Christian Parpart <trapni@gmail.com>
2014-06-10 09:11:31 +0200

    [x0d] echo_example adapted to recent API changes

--- x0d/plugins/echo_example.cpp
+++ x0d/plugins/echo_example.cpp
@@ -7,80 +7,10 @@
  */
 
 #include <x0d/XzeroPlugin.h>
-#include <x0/http/HttpServer.h>
 #include <x0/http/HttpRequest.h>
-#include <x0/http/HttpHeader.h>
-#include <x0/io/BufferSource.h>
 #include <x0/io/BufferRefSource.h>
-#include <x0/strutils.h>
-#include <x0/Types.h>
 
-#define TRACE(msg...) DEBUG("echo: " msg)
-
-class EchoHandler
-{
-private:
-    x0::HttpRequest* request_;
-
-public:
-    EchoHandler(x0::HttpRequest* in) :
-        request_(in)
-    {
-    }
-
-    void run()
-    {
-        // set response status code
-        request_->status = x0::HttpStatus::Ok;
-
-        // set response header "Content-Length",
-        // if request content were not encoded
-        // and if we've received its request header "Content-Length"
-        if (!request_->requestHeader("Content-Encoding"))
-            if (x0::BufferRef value = request_->requestHeader("Content-Length"))
-                request_->responseHeaders.overwrite("Content-Length", value.str());
-
-        // try to read content (if available) and pass it on to our onContent handler,
-        // or fall back to just write HELLO (if no request content body was sent).
-
-        if (request_->contentAvailable()) {
-            request_->setBodyCallback<EchoHandler, &EchoHandler::onContent>(this);
-        } else {
-            request_->write<x0::BufferSource>("I'm an HTTP echo-server, dude.\n");
-            request_->finish();
-            delete this;
-        }
-    }
-
-private:
-    // Handler, invoked on request content body chunks,
-    // which we want to "echo" back to the client.
-    //
-    // NOTE, this can be invoked multiple times, depending on the input.
-    void onContent(const x0::BufferRef& chunk)
-    {
-        TRACE("onContent('%s')", chunk.str().c_str());
-        request_->write<x0::BufferRefSource>(std::move(chunk));
-        request_->writeCallback<EchoHandler, &EchoHandler::contentWritten>(this);
-    }
-
-    // Handler, invoked when a content chunk has been fully written to the client
-    // (or an error occurred).
-    //
-    // We will try to read another input chunk to echo back to the client,
-    // or just finish the response if failed.
-    void contentWritten()
-    {
-        // TODO (write-)error handling; pass errno to this fn, too.
-
-        // is there more data available?
-        if (!request_->contentAvailable()) {
-            // could not read another input chunk, so finish processing this request.
-            request_->finish();
-            delete this;
-        }
-    }
-};
+using namespace x0;
 
 /**
  * \ingroup plugins
@@ -101,10 +31,17 @@ public:
     }
 
 private:
-    virtual bool handleRequest(x0::HttpRequest *in, x0::FlowVM::Params& args)
+    virtual bool handleRequest(HttpRequest* r, FlowVM::Params& args)
     {
-        // create a handler serving this very request.
-        (new EchoHandler(in))->run();
+        r->status = HttpStatus::Ok;
+
+        if (r->contentAvailable()) {
+            r->write(std::move(r->consumeBody()));
+            r->finish();
+        } else {
+            r->write<BufferRefSource>(BufferRef("I'm an HTTP echo-server, dude.\n"));
+            r->finish();
+        }
 
         // yes, we are handling this request
         return true;
