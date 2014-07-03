ec923f7e4489417ad0724379412e40522a5ff5b4
Christian Parpart <trapni@gmail.com>
2014-07-03 16:10:57 +0200

    ppa-release.sh script update

--- contrib/ppa-release.sh
+++ contrib/ppa-release.sh
@@ -1,16 +1,23 @@
 #! /bin/bash
 # vim:ts=2:sw=2
 
+#PPA="xzero-precise"
+#PPA="xzero-dev"
+PPA="xzero"
+
 # switch to project root
 cd $(dirname $0)/..
 
 VERSION=`head -n1 debian/changelog | sed 's/^.*(\(.*\)).*$/\1/'`
 
-# always clean before building as dpkg-source will badly include just everything (FIXME: can we provide it a blacklist?)
+echo "PPA package version: ${VERSION}"
+
+# always clean before building as dpkg-source will badly include just
+# everything (FIXME: can we provide it a blacklist?)
 ./autogen.sh clean
 
 # build source-only
 dpkg-buildpackage -S || exit $?
 
 # upload to PPA
-dput -f ppa:trapni/xzero-dev ../x0_${VERSION}_source.changes || exit $?
+dput -f ppa:trapni/${PPA} ../x0_${VERSION}_source.changes || exit $?
