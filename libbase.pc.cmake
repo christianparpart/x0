3f37142a6bd9abe5b81ef0708062708a0d104d92
Christian Parpart <trapni@gmail.com>
2014-07-26 01:58:15 +0200

    want libev, too

--- libbase.pc.cmake
+++ libbase.pc.cmake
@@ -5,5 +5,5 @@ Description: C++ base framework, containing general-purpose APIs
 Version: @LIBBASE_VERSION@
 Requires: libpcre >= 7.0 
 # Conflicts: 
-Libs: -L@CMAKE_INSTALL_PREFIX@/lib -lbase -lpthread -pthread @POSIX_RT_LIBS@ @MYSQL_LDFLAGS@
+Libs: -L@CMAKE_INSTALL_PREFIX@/lib -lbase -lev -lpthread -pthread @POSIX_RT_LIBS@ @MYSQL_LDFLAGS@
 Cflags: -I@CMAKE_INSTALL_PREFIX@/include -pthread @CFLAGS_RDYNAMIC@ @MYSQL_CFLAGS@ @CXXFLAGS@
