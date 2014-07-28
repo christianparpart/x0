8b7440bc0fc9c85757a151d586ed8b3eb6171919
Christian Parpart <trapni@gmail.com>
2014-07-28 23:20:43 +0200

    libxzero: adds an example for serving static files.

---
+++ examples/hello_staticfile.cpp
@@ -0,0 +1,52 @@
+// This file is part of the "x0" project
+//   (c) 2009-2014 Christian Parpart <trapni@gmail.com>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
+/**
+ * HTTP Server example for serving local static files.
+ *
+ * Highlights:
+ *
+ * This code has builtin support for:
+ * <ul>
+ *   <li> GET and HEAD requests </li>
+ *   <li> client cache aware response handling </li>
+ *   <li> Range requests </li>
+ * </ul>
+ *
+ */
+
+#include <xzero/HttpRequest.h>
+#include <xzero/HttpServer.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <ev++.h>
+
+int main() {
+  xzero::HttpServer httpServer(ev::default_loop(0));
+  httpServer.setupListener("0.0.0.0", 3000);
+
+  char cwd[1024];
+  if (!getcwd(cwd, sizeof(cwd))) {
+    cwd[0] = '.';
+    cwd[1] = '\0';
+  }
+
+  printf("Serving HTTP from 0.0.0.0:3000 ...\n");
+
+  httpServer.requestHandler = [&](xzero::HttpRequest* r) {
+    r->documentRoot = cwd;
+    r->fileinfo = r->connection.worker().fileinfo(r->documentRoot + r->path);
+    if (r->fileinfo) {
+      r->sendfile(r->fileinfo);
+    } else {
+      r->status = xzero::HttpStatus::NotFound;
+    }
+    r->finish();
+  };
+
+  return httpServer.run();
+}
