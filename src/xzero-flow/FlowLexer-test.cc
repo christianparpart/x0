685e2286a707b7ed3eaa33e58679783bdd22f9f2
Christian Parpart <trapni@gmail.com>
2015-10-20 18:31:52 +0200

    initial import

---
+++ src/xzero-flow/FlowLexer-test.cc
@@ -0,0 +1,47 @@
+// TODO
+
+#include <xzero-flow/FlowLexer.h>
+#include <xzero-flow/FlowToken.h>
+#include <gtest/gtest.h>
+
+using namespace xzero;
+using namespace xzero::flow;
+
+TEST(FlowLexer, eof) {
+  FlowLexer lexer;
+  lexer.openString("");
+
+  ASSERT_EQ(FlowToken::Eof, lexer.token());
+  ASSERT_EQ(FlowToken::Eof, lexer.nextToken());
+  ASSERT_EQ(FlowToken::Eof, lexer.token());
+  ASSERT_EQ(1, lexer.line());
+  ASSERT_EQ(1, lexer.column());
+}
+
+TEST(FlowLexer, token_keywords) {
+  FlowLexer lexer;
+  lexer.openString("handler");
+
+  ASSERT_EQ(FlowToken::Handler, lexer.token());
+  ASSERT_EQ(1, lexer.line());
+  ASSERT_EQ(7, lexer.column());
+}
+
+TEST(FlowLexer, composed) {
+  FlowLexer lexer;
+  lexer.openString("handler main {}");
+
+  ASSERT_EQ(FlowToken::Handler, lexer.token());
+  ASSERT_EQ("handler", lexer.stringValue());
+
+  ASSERT_EQ(FlowToken::Ident, lexer.nextToken());
+  ASSERT_EQ("main", lexer.stringValue());
+
+  ASSERT_EQ(FlowToken::Begin, lexer.nextToken());
+
+  ASSERT_EQ(FlowToken::End, lexer.nextToken());
+
+  ASSERT_EQ(FlowToken::Eof, lexer.nextToken());
+
+  ASSERT_EQ(FlowToken::Eof, lexer.nextToken());
+}
---
+++ src/xzero-flow/ir/HandlerPass.h
@@ -0,0 +1,38 @@
+// This file is part of the "x0" project, http://xzero.io/
+//   (c) 2009-2014 Christian Parpart <trapni@gmail.com>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
+#pragma once
+
+#include <xzero-flow/Api.h>
+
+namespace xzero {
+namespace flow {
+
+class IRHandler;
+
+class XZERO_FLOW_API HandlerPass {
+ public:
+  explicit HandlerPass(const char* name) : name_(name) {}
+
+  virtual ~HandlerPass() {}
+
+  /**
+   * Runs this pass on given \p handler.
+   *
+   * @retval true The handler was modified.
+   * @retval false The handler was not modified.
+   */
+  virtual bool run(IRHandler* handler) = 0;
+
+  const char* name() const { return name_; }
+
+ private:
+  const char* name_;
+};
+
+}  // namespace flow
+}  // namespace xzero
