90f873093f69bbf3f6f286f81d32b9cdedd1caa4
Christian Parpart <trapni@gmail.com>
2015-12-20 21:33:04 +0100

    get examples back to life. well, at least one, but it's a start. ;-)

---
+++ src/examples/hello_example.cc
@@ -0,0 +1,62 @@
+// This file is part of the "x0" project, http://xzero.io/
+//   (c) 2009-2014 Christian Parpart <trapni@gmail.com>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
+#include <xzero/http/HttpService.h>
+#include <xzero/http/HttpRequest.h>
+#include <xzero/http/HttpResponse.h>
+#include <xzero/executor/PosixScheduler.h>
+
+using namespace xzero;
+using namespace xzero::http;
+
+class HelloService : public HttpService::Handler {
+ public:
+  bool handleRequest(HttpRequest* request, HttpResponse* response) override;
+};
+
+bool HelloService::handleRequest(HttpRequest* request, HttpResponse* response) {
+  // set response status code
+  response->setStatus(HttpStatus::Ok);
+
+  // set some custom response header
+  response->addHeader("Hello", "World");
+
+  // write some content to the client
+  response->write("Hello, World!\n");
+
+  // invoke completed() marking the response as being "complete",
+  // i.e. fully generated.
+  response->completed();
+
+  // Tell the caller that we've handled the request,
+  // otherwise another handler will be tried.
+  return true;
+}
+
+int main(int argc, const char* argv[]) {
+  PosixScheduler scheduler;
+  HttpService service(HttpService::HTTP1);
+
+  IPAddress bind = IPAddress("127.0.0.1");
+  int port = 3000;
+
+  HelloService hello;
+  service.addHandler(&hello);
+
+  service.configureInet(&scheduler, &scheduler,
+                        20_seconds, // read timeout
+                        10_seconds, // write timeout
+                        8_seconds, // TCP FIN timeout
+                        bind,
+                        port);
+
+  service.start();
+
+  scheduler.runLoop();
+
+  return 0;
+}
