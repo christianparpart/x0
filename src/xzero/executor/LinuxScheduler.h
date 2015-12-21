2f0fee2aa970355a60cae8018966ea69d80d5892
Christian Parpart <trapni@gmail.com>
2015-12-21 02:13:06 +0100

    LinuxScheduler: partial brain upload

--- src/xzero/executor/LinuxScheduler.h
+++ src/xzero/executor/LinuxScheduler.h
@@ -11,6 +11,7 @@
 
 #include <xzero/Api.h>
 #include <xzero/RefPtr.h>
+#include <xzero/io/FileDescriptor.h>
 #include <xzero/executor/Scheduler.h>
 #include <sys/select.h>
 #include <set>
@@ -20,8 +21,6 @@
 
 namespace xzero {
 
-class WallClock;
-
 /**
  * Implements Scheduler Executor API via native Linux features,
  * such as @c epoll, @c timerfd, @c eventfd, etc.
@@ -30,17 +29,17 @@ class XZERO_BASE_API LinuxScheduler : public Scheduler {
  public:
   LinuxScheduler(
       std::function<void(const std::exception&)> errorLogger,
-      WallClock* clock,
       std::function<void()> preInvoke,
       std::function<void()> postInvoke);
 
   explicit LinuxScheduler(
-      std::function<void(const std::exception&)> errorLogger,
-      WallClock* clock);
+      std::function<void(const std::exception&)> errorLogger);
 
   LinuxScheduler();
 
   ~LinuxScheduler();
+
+  MonotonicTime now() const;
 
   using Scheduler::executeOnReadable;
   using Scheduler::executeOnWritable;
@@ -51,6 +50,7 @@ class XZERO_BASE_API LinuxScheduler : public Scheduler {
   HandleRef executeAt(UnixTime dt, Task task) override;
   HandleRef executeOnReadable(int fd, Task task, Duration tmo, Task tcb) override;
   HandleRef executeOnWritable(int fd, Task task, Duration tmo, Task tcb) override;
+  HandleRef executeOnSignal(int signo, SignalHandler task) override;
   void executeOnWakeup(Task task, Wakeup* wakeup, long generation) override;
   size_t timerCount() override;
   size_t readerCount() override;
@@ -60,27 +60,7 @@ class XZERO_BASE_API LinuxScheduler : public Scheduler {
   void runLoopOnce() override;
   void breakLoop() override;
 
-  /**
-   * Waits at most @p timeout for @p fd to become readable without blocking.
-   */
-  static void waitForReadable(int fd, Duration timeout);
-
-  /**
-   * Waits until given @p fd becomes readable without blocking.
-   */
-  static void waitForReadable(int fd);
-
-  /**
-   * Waits at most @p timeout for @p fd to become writable without blocking.
-   */
-  static void waitForWritable(int fd, Duration timeout);
-
-  /**
-   * Waits until given @p fd becomes writable without blocking.
-   */
-  static void waitForWritable(int fd);
-
- public:
+ protected:
   enum class Mode { READABLE, WRITABLE };
   struct Watcher : public Handle { // {{{
     int fd;
@@ -145,68 +125,20 @@ class XZERO_BASE_API LinuxScheduler : public Scheduler {
     Timer& operator=(const Timer&) = default;
   }; // }}}
 
- protected:
-  /**
-   * Adds given timer-handle to the timer-list.
-   *
-   * @param dt timestamp at which given timer is to be fired.
-   * @param task task to invoke upon fire.
-   *
-   * @note The caller must protect the access itself.
-   */
-  HandleRef insertIntoTimersList(UnixTime dt, Task task);
+ private:
+  Watcher* linkWatcher(Watcher* w, Watcher* pred);
+  Watcher* unlinkWatcher(Watcher* w);
+  HandleRef insertTimer(MonotonicTime time, Task task);
 
   void collectTimeouts(std::list<Task>* result);
 
-  void collectActiveHandles(const fd_set* input,
-                            const fd_set* output,
-                            std::list<Task>* result);
-
-  /**
-   * Registers an I/O interest.
-   *
-   * @note requires the caller to lock the object mutex.
-   */
-  HandleRef setupWatcher(int fd, Mode mode, Task onFire,
-                         Duration timeout, Task onTimeout);
-
-  /**
-   * Inserts watcher between @p pred and @p pred's successor.
-   *
-   * @note requires the caller to lock the object mutex.
-   */
-  void linkWatcher(Watcher* w, Watcher* pred);
-
-  /**
-   * Removes given watcher from ordered list of watchers.
-   *
-   * @return watcher next to the given watcher, ascending ordered by time.
-   * @note requires the caller to lock the object mutex.
-   */
-  Watcher* unlinkWatcher(Watcher* w);
-
-  /**
-   * Retrieves a watcher by file descriptor.
-   *
-   * @note requires the caller to lock the object mutex.
-   */
-  Watcher* getWatcher(int fd);
-
-  /**
-   * Computes the timespan the event loop should wait the most.
-   *
-   * @note requires the caller to lock the object mutex.
-   */
-  Duration nextTimeout() const;
-
-  std::string inspectImpl() const;
-
-  friend std::string inspect(const LinuxScheduler&);
-
  private:
-  WallClock* clock_;
   Task onPreInvokePending_;  //!< callback to be invoked before any other hot CB
   Task onPostInvokePending_; //!< callback to be invoked after any other hot CB
+
+  FileDescriptor epollfd_;
+  FileDescriptor eventfd_;
+  FileDescriptor signalfd_;
 
   std::atomic<size_t> readerCount_;
   std::atomic<size_t> writerCount_;
