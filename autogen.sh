57abdb41e22bc587b5c38d5d3b0ad5f629edb972
Christian Parpart <trapni@gmail.com>
2016-07-06 00:07:11 +0200

    autogen.sh daily-use improvements

--- autogen.sh
+++ autogen.sh
@@ -21,10 +21,22 @@ if test "$1" == "clean"; then
   exit 0
 fi
 
-exec autoreconf --verbose --force --install $ROOT
+autoreconf --verbose --force --install $ROOT
 
-glibtoolize --force
-aclocal
-autoheader
-automake --force-missing --add-missing
-autoconf
+findexe() {
+  for exe in ${@}; do
+    if which $exe &>/dev/null; then
+      echo $exe
+      return
+    fi
+  done
+  echo $1
+}
+
+export CXX=$(findexe clang++-3.5 clang++)
+export CC=$(findexe clang-3.5 clang)
+
+echo CXX = $CXX
+echo CC = $CC
+
+$ROOT/configure --prefix="$HOME/local"
