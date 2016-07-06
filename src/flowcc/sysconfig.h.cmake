937508441fa136ec196e85461117d27dc82dff92
Christian Parpart <trapni@gmail.com>
2016-07-06 08:50:07 +0200

    cmake cleanup

--- src/flowcc/sysconfig.h.cmake
+++
@@ -1,8 +0,0 @@
-// This file is part of the "x0" project
-//   (c) 2009-2015 Christian Parpart <https://github.com/christianparpart>
-//
-// x0 is free software: you can redistribute it and/or modify it under
-// the terms of the GNU Affero General Public License v3.0.
-// You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
-#pragma once
