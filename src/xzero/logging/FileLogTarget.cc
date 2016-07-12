417e3626c2e3c2658c71cf8e4b1484b09d598239
Christian Parpart <trapni@gmail.com>
2016-07-12 04:27:09 +0200

    [xzero] logging: adds FileLogTarget

---
+++ src/xzero/logging/FileLogTarget.cc
@@ -0,0 +1,45 @@
+// This file is part of the "x0" project, http://github.com/christianparpart/x0>
+//   (c) 2009-2016 Christian Parpart <trapni@gmail.com>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
+#include <xzero/logging/FileLogTarget.h>
+#include <xzero/logging/LogLevel.h>
+#include <xzero/StringUtil.h>
+#include <xzero/logging.h>
+#include <xzero/WallClock.h>
+#include <xzero/UnixTime.h>
+#include <unistd.h>
+
+namespace xzero {
+
+FileLogTarget::FileLogTarget(std::unique_ptr<OutputStream>&& output)
+    : output_(std::move(output)),
+      timestampEnabled_(true) {
+}
+
+// TODO is a mutex required for concurrent printf()'s ?
+void FileLogTarget::log(LogLevel level,
+                           const String& component,
+                           const String& message) {
+  std::string logline = StringUtil::format(
+      "$0[$1] [$2] $3\n",
+      createTimestamp(),
+      level,
+      component,
+      message);
+
+  output_->write(logline.data(), logline.size());
+}
+
+std::string FileLogTarget::createTimestamp() const {
+  if (timestampEnabled_ == false)
+    return "";
+
+  return WallClock::now().toString("%Y-%m-%d %H:%M:%S ");
+}
+
+} // namespace xzero
+
---
+++ src/xzero/logging/FileLogTarget.h
@@ -0,0 +1,35 @@
+// This file is part of the "x0" project, http://github.com/christianparpart/x0>
+//   (c) 2009-2016 Christian Parpart <trapni@gmail.com>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+#pragma once
+
+#include <xzero/logging/LogTarget.h>
+#include <xzero/io/OutputStream.h>
+#include <memory>
+
+namespace xzero {
+
+class FileLogTarget : public LogTarget {
+public:
+  explicit FileLogTarget(std::unique_ptr<OutputStream>&& output);
+
+  void log(
+      LogLevel level,
+      const String& component,
+      const String& message) override;
+
+  void setTimestampEnabled(bool value) { timestampEnabled_ = value; }
+  bool isTimestampEnabled() const noexcept { return timestampEnabled_; }
+
+private:
+  std::string createTimestamp() const;
+
+private:
+  std::unique_ptr<OutputStream> output_;
+  bool timestampEnabled_;
+};
+
+} // namespace xzero
