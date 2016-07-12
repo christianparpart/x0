89f47b8b8fc40c47ba7a6873195adff113a347f2
Christian Parpart <trapni@gmail.com>
2016-07-12 04:29:19 +0200

    [xzero] adds SystemPipe as public API

---
+++ src/xzero/io/SystemPipe.cc
@@ -0,0 +1,34 @@
+// This file is part of the "x0" project, http://github.com/christianparpart/x0>
+//   (c) 2009-2016 Christian Parpart <trapni@gmail.com>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
+#include <xzero/io/SystemPipe.h>
+#include <xzero/RuntimeError.h>
+#include <unistd.h>
+
+namespace xzero {
+
+SystemPipe::SystemPipe(int reader, int writer) {
+  fds_[READER] = reader;
+  fds_[WRITER] = writer;
+}
+
+SystemPipe::SystemPipe() : SystemPipe(-1, -1) {
+  if (pipe(fds_) < 0) {
+    RAISE_ERRNO(errno);
+  }
+}
+
+SystemPipe::~SystemPipe() {
+  closeReader();
+  closeWriter();
+}
+
+int SystemPipe::write(const std::string& msg) {
+  return ::write(writerFd(), msg.data(), msg.size());
+}
+
+} // namespace xzero
---
+++ src/xzero/io/SystemPipe.h
@@ -0,0 +1,56 @@
+// This file is part of the "x0" project, http://github.com/christianparpart/x0>
+//   (c) 2009-2016 Christian Parpart <trapni@gmail.com>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+#pragma once
+
+#include <string>
+#include <unistd.h>
+
+namespace xzero {
+
+class SystemPipe {
+ private:
+  enum {
+    READER = 0,
+    WRITER = 1,
+  };
+
+ public:
+  SystemPipe(int reader, int writer);
+  SystemPipe();
+  ~SystemPipe();
+
+  int write(const std::string& msg);
+
+  bool isValid() const noexcept { return fds_[READER] != -1; }
+  int readerFd() const noexcept { return fds_[READER]; }
+  int writerFd() const noexcept { return fds_[WRITER]; }
+
+  void closeReader();
+  void closeWriter();
+
+ private:
+  void closeEndPoint(int index);
+
+ private:
+  int fds_[2];
+};
+
+inline void SystemPipe::closeReader() {
+  closeEndPoint(READER);
+}
+
+inline void SystemPipe::closeWriter() {
+  closeEndPoint(WRITER);
+}
+
+inline void SystemPipe::closeEndPoint(int index) {
+  if (fds_[index] != -1) {
+    ::close(fds_[index]);
+  }
+}
+
+} // namespace xzero
