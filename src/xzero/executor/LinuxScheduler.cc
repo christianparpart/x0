d1b1e28a8f1166c86a747fa0d946fec39e381187
Christian Parpart <trapni@gmail.com>
2016-07-25 18:17:42 +0200

    [xzero] LinuxScheduler: WIP commit

--- src/xzero/executor/LinuxScheduler.cc
+++ src/xzero/executor/LinuxScheduler.cc
@@ -6,36 +6,80 @@
 // the License at: http://opensource.org/licenses/MIT
 
 #include <xzero/executor/LinuxScheduler.h>
+#include <xzero/MonotonicTime.h>
+#include <xzero/MonotonicClock.h>
 #include <xzero/WallClock.h>
+
+#include <sys/epoll.h>
+#include <sys/timerfd.h>
+#include <sys/eventfd.h>
+#include <sys/signalfd.h>
 
 namespace xzero {
 
 LinuxScheduler::LinuxScheduler(
-    std::function<void(const std::exception&)> errorLogger,
-    WallClock* clock,
+    std::unique_ptr<xzero::ExceptionHandler> eh,
     std::function<void()> preInvoke,
     std::function<void()> postInvoke)
-    : Scheduler(errorLogger), 
-      clock_(clock ? clock : WallClock::monotonic()),
+    : EventLoop(std::move(eh)), 
       onPreInvokePending_(preInvoke),
       onPostInvokePending_(postInvoke),
+      epollfd_(),
+      eventfd_(),
+      timerfd_(),
+      signalfd_(),
       readerCount_(0),
       writerCount_(0)
 {
 }
 
 LinuxScheduler::LinuxScheduler(
-    std::function<void(const std::exception&)> errorLogger,
-    WallClock* clock)
-    : LinuxScheduler(errorLogger, clock, nullptr, nullptr) {
+    std::unique_ptr<xzero::ExceptionHandler> eh)
+    : LinuxScheduler(std::move(eh), nullptr, nullptr) {
 }
 
 LinuxScheduler::LinuxScheduler()
-    : LinuxScheduler(nullptr, nullptr) {
+    : LinuxScheduler(nullptr, nullptr, nullptr) {
 }
 
 LinuxScheduler::~LinuxScheduler() {
 }
 
+MonotonicTime LinuxScheduler::now() const {
+  return MonotonicClock::now();
+}
+
+void LinuxScheduler::execute(Task task) {
+}
+
+std::string LinuxScheduler::toString() const {
+}
+
+Executor::HandleRef LinuxScheduler::executeAfter(Duration delay, Task task) {
+}
+
+Executor::HandleRef LinuxScheduler::executeAt(UnixTime dt, Task task) {
+}
+
+Executor::HandleRef LinuxScheduler::executeOnReadable(int fd, Task task, Duration tmo, Task tcb) {
+}
+
+Executor::HandleRef LinuxScheduler::executeOnWritable(int fd, Task task, Duration tmo, Task tcb) {
+}
+
+void LinuxScheduler::cancelFD(int fd) {
+}
+
+void LinuxScheduler::executeOnWakeup(Task task, Wakeup* wakeup, long generation) {
+}
+
+void LinuxScheduler::runLoop() {
+}
+
+void LinuxScheduler::runLoopOnce() {
+}
+
+void LinuxScheduler::breakLoop() {
+}
 
 } // namespace xzero
--- src/xzero/executor/LinuxScheduler.h
+++ src/xzero/executor/LinuxScheduler.h
@@ -9,12 +9,13 @@
 
 #include <xzero/Api.h>
 #include <xzero/RefPtr.h>
+#include <xzero/MonotonicTime.h>
+#include <xzero/executor/EventLoop.h>
 #include <xzero/io/FileDescriptor.h>
-#include <xzero/executor/Scheduler.h>
-#include <sys/select.h>
-#include <set>
 #include <vector>
+#include <set>
 #include <list>
+#include <memory>
 #include <mutex>
 
 namespace xzero {
@@ -23,24 +24,24 @@ namespace xzero {
  * Implements Scheduler Executor API via native Linux features,
  * such as @c epoll, @c timerfd, @c eventfd, etc.
  */
-class XZERO_BASE_API LinuxScheduler : public Scheduler {
+class XZERO_BASE_API LinuxScheduler : public EventLoop {
  public:
   LinuxScheduler(
-      std::function<void(const std::exception&)> errorLogger,
+      std::unique_ptr<ExceptionHandler> eh,
       std::function<void()> preInvoke,
       std::function<void()> postInvoke);
 
   explicit LinuxScheduler(
-      std::function<void(const std::exception&)> errorLogger);
+      std::unique_ptr<ExceptionHandler> eh);
 
   LinuxScheduler();
-
   ~LinuxScheduler();
 
   MonotonicTime now() const;
 
-  using Scheduler::executeOnReadable;
-  using Scheduler::executeOnWritable;
+ public:
+  using EventLoop::executeOnReadable;
+  using EventLoop::executeOnWritable;
 
   void execute(Task task) override;
   std::string toString() const override;
@@ -48,87 +49,15 @@ class XZERO_BASE_API LinuxScheduler : public Scheduler {
   HandleRef executeAt(UnixTime dt, Task task) override;
   HandleRef executeOnReadable(int fd, Task task, Duration tmo, Task tcb) override;
   HandleRef executeOnWritable(int fd, Task task, Duration tmo, Task tcb) override;
-  HandleRef executeOnSignal(int signo, SignalHandler task) override;
+  void cancelFD(int fd) override;
   void executeOnWakeup(Task task, Wakeup* wakeup, long generation) override;
-  size_t timerCount() override;
-  size_t readerCount() override;
-  size_t writerCount() override;
-  size_t taskCount() override;
+
   void runLoop() override;
   void runLoopOnce() override;
   void breakLoop() override;
 
- protected:
-  enum class Mode { READABLE, WRITABLE };
-  struct Watcher : public Handle { // {{{
-    int fd;
-    Mode mode;
-    Task onIO;
-    UnixTime timeout;
-    Task onTimeout;
-
-    Watcher* prev; //!< predecessor by timeout ASC
-    Watcher* next; //!< successor by timeout ASC
-
-    Watcher()
-        : Watcher(-1, Mode::READABLE, nullptr, UnixTime(0.0), nullptr) {}
-
-    Watcher(const Watcher& w)
-        : Watcher(w.fd, w.mode, w.onIO, w.timeout, w.onTimeout) {}
-
-    Watcher(int _fd, Mode _mode, Task _onIO,
-            UnixTime _timeout, Task _onTimeout)
-        : fd(_fd), mode(_mode), onIO(_onIO),
-          timeout(_timeout), onTimeout(_onTimeout),
-          prev(nullptr), next(nullptr) {
-      // Manually ref because we're not holding it in a
-      // RefPtr<Watcher> vector in LinuxScheduler.
-      // - Though, no need to manually unref() either.
-      ref();
-    }
-
-    void reset(int _fd, Mode _mode, Task _onIO,
-            UnixTime _timeout, Task _onTimeout) {
-      fd = _fd;
-      mode = _mode;
-      onIO = _onIO;
-      timeout = _timeout;
-      onTimeout = _onTimeout;
-
-      prev = nullptr;
-      next = nullptr;
-
-      Handle::reset(nullptr);
-    }
-
-    void clear() {
-      fd = -1;
-      timeout = UnixTime(0.0);
-      prev = nullptr;
-      next = nullptr;
-    }
-
-    bool operator<(const Watcher& other) const {
-      return timeout < other.timeout;
-    }
-  }; // }}}
-  struct Timer : public Handle { // {{{
-    UnixTime when;
-    Task action;
-
-    Timer() : Handle(), when(), action() {}
-    Timer(UnixTime dt, Task t) : Handle(), when(dt), action(t) {}
-    Timer(UnixTime dt, Task t, Task c) : Handle(c), when(dt), action(t) {}
-    Timer(const Timer&) = default;
-    Timer& operator=(const Timer&) = default;
-  }; // }}}
-
  private:
-  Watcher* linkWatcher(Watcher* w, Watcher* pred);
-  Watcher* unlinkWatcher(Watcher* w);
-  HandleRef insertTimer(MonotonicTime time, Task task);
-
-  void collectTimeouts(std::list<Task>* result);
+  // helper
 
  private:
   Task onPreInvokePending_;  //!< callback to be invoked before any other hot CB
@@ -136,14 +65,15 @@ class XZERO_BASE_API LinuxScheduler : public Scheduler {
 
   FileDescriptor epollfd_;
   FileDescriptor eventfd_;
+  FileDescriptor timerfd_;
   FileDescriptor signalfd_;
 
   std::atomic<size_t> readerCount_;
   std::atomic<size_t> writerCount_;
 };
 
-XZERO_BASE_API std::string inspect(LinuxScheduler::Mode mode);
-XZERO_BASE_API std::string inspect(const LinuxScheduler::Watcher& w);
-XZERO_BASE_API std::string inspect(const LinuxScheduler& s);
+// std::string inspect(LinuxScheduler::Mode mode);
+// std::string inspect(const LinuxScheduler::Watcher& w);
+// std::string inspect(const LinuxScheduler& s);
 
 } // namespace xzero
