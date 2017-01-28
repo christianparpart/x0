887d894219182fdb005da82b61c29c1d73b4ae2f
Christian Parpart <trapni@gmail.com>
2017-01-28 02:43:48 +0100

    updated copyright year to 2017

--- contrib/x0d.bash-completion.sh
+++ contrib/x0d.bash-completion.sh
@@ -1,5 +1,5 @@
 # This file is part of the "x0" project, http://xzero.io/
-#   (c) 2009-2014 Christian Parpart <christian@parpart.family>
+#   (c) 2009-2017 Christian Parpart <christian@parpart.family>
 #
 # Licensed under the MIT License (the "License"); you may not use this
 # file except in compliance with the License. You may obtain a copy of
