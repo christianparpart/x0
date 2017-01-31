3870ecac2d5977f35a2bc700b6bac4ba49dff462
Christian Parpart <trapni@gmail.com>
2017-01-31 01:32:47 +0100

    [xzero] raft: adds MessageType

---
+++ src/xzero/raft/MessageType.cc
@@ -0,0 +1,32 @@
+// This file is part of the "x0" project, http://github.com/christianparpart/x0>
+//   (c) 2009-2017 Christian Parpart <christian@parpart.family>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+#include <xzero/raft/MessageType.h>
+#include <xzero/StringUtil.h>
+
+namespace xzero {
+
+using raft::MessageType;
+
+template<>
+std::string StringUtil::toString<>(const MessageType type) {
+  switch (type) {
+    case MessageType::VoteRequest:
+      return "VoteRequest";
+    case MessageType::VoteResponse:
+      return "VoteResponse";
+    case MessageType::AppendEntriesRequest:
+      return "AppendEntriesRequest";
+    case MessageType::AppendEntriesResponse:
+      return "AppendEntriesResponse";
+    case MessageType::InstallSnapshotRequest:
+      return "InstallSnapshotRequest";
+    case MessageType::InstallSnapshotResponse:
+      return "InstallSnapshotResponse";
+  }
+}
+
+} // namespace xzero
---
+++ src/xzero/raft/MessageType.h
@@ -0,0 +1,24 @@
+// This file is part of the "x0" project, http://github.com/christianparpart/x0>
+//   (c) 2009-2017 Christian Parpart <christian@parpart.family>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+#pragma once
+
+#include <cstdint>
+
+namespace xzero {
+namespace raft {
+
+enum class MessageType : uint8_t {
+  VoteRequest = 1,
+  VoteResponse = 2,
+  AppendEntriesRequest = 3,
+  AppendEntriesResponse = 4,
+  InstallSnapshotRequest = 5,
+  InstallSnapshotResponse = 6,
+};
+
+} // namespace raft
+} // namespace xzero
