ed7ca00e03bfe2ca2f4262b78cfd503acec8cb8c
Christian Parpart <trapni@gmail.com>
2017-01-31 22:01:44 +0100

    [xzero] BinaryWriter: code cleanup

--- src/xzero/util/BinaryWriter-test.cc
+++ src/xzero/util/BinaryWriter-test.cc
@@ -22,7 +22,7 @@ TEST(util_BinaryWriter, writeVarUInt_300) {
   Buffer out;
   BinaryWriter g(BufferUtil::writer(&out));
   g.writeVarUInt(300);
-  logf("varint(300) = $0", BufferUtil::hexPrint(&out));
+  logf("varint(300) = $0", out);
   ASSERT_EQ("10101100 00000010", BufferUtil::binPrint(out, true));
 }
 
@@ -30,7 +30,7 @@ TEST(util_BinaryWriter, writeVarSInt64) {
   Buffer out;
   BinaryWriter g(BufferUtil::writer(&out));
   g.writeVarSInt64(-300);
-  logf("encoded = $0", BufferUtil::hexPrint(&out));
+  logf("encoded = $0", out);
   EXPECT_EQ("\xd7\x04", out);
 }
 
@@ -38,7 +38,7 @@ TEST(util_BinaryWriter, writeVarSInt32) {
   Buffer out;
   BinaryWriter g(BufferUtil::writer(&out));
   g.writeVarSInt32(-300);
-  logf("encoded = $0", BufferUtil::hexPrint(&out));
+  logf("encoded = $0", out);
   EXPECT_EQ("\xd7\x04", out);
 }
 
@@ -46,7 +46,7 @@ TEST(util_BinaryWriter, writeLengthDelimited) {
   Buffer out;
   BinaryWriter g(BufferUtil::writer(&out));
   g.writeLengthDelimited((const uint8_t*) "testing", 7);
-  logf("encoded = $0", BufferUtil::hexPrint(&out));
+  logf("encoded = $0", out);
   EXPECT_EQ("\x07\x74\x65\x73\x74\x69\x6e\x67", out);
 }
 
@@ -54,7 +54,7 @@ TEST(util_BinaryWriter, writeFixed64) {
   Buffer out;
   BinaryWriter g(BufferUtil::writer(&out));
   g.writeFixed64(0x0011223344556677llu);
-  logf("encoded = $0", BufferUtil::hexPrint(&out));
+  logf("encoded = $0", out);
   EXPECT_EQ("\x77\x66\x55\x44\x33\x22\x11\x00", out);
 }
 
@@ -62,7 +62,7 @@ TEST(util_BinaryWriter, writeFixed32) {
   Buffer out;
   BinaryWriter g(BufferUtil::writer(&out));
   g.writeFixed32(0x12345678);
-  logf("encoded = $0", BufferUtil::hexPrint(&out));
+  logf("encoded = $0", out);
   EXPECT_EQ("\x78\x56\x34\x12", out);
 }
 
@@ -70,7 +70,7 @@ TEST(util_BinaryWriter, writeDouble) {
   Buffer out;
   BinaryWriter g(BufferUtil::writer(&out));
   g.writeDouble(3.14);
-  logf("encoded = $0", BufferUtil::hexPrint(&out));
+  logf("encoded = $0", out);
   EXPECT_EQ("\x1f\x85\xeb\x51\xb8\x1e\x09\x40", out);
 }
 
@@ -78,7 +78,7 @@ TEST(util_BinaryWriter, writeFloat) {
   Buffer out;
   BinaryWriter g(BufferUtil::writer(&out));
   g.writeFloat(3.14f);
-  logf("encoded = $0", BufferUtil::hexPrint(&out));
+  logf("encoded = $0", out);
   EXPECT_EQ("\xc3\xf5\x48\x40", out);
 }
 
@@ -86,6 +86,6 @@ TEST(util_BinaryWriter, writeString) {
   Buffer out;
   BinaryWriter g(BufferUtil::writer(&out));
   g.writeString("testing");
-  logf("encoded = $0", BufferUtil::hexPrint(&out));
-  EXPECT_EQ("\x07\x74\x65\x73\x74\x69\x6e\x67", out);
+  logf("encoded = $0", out);
+  EXPECT_EQ("\x07testing", out);
 }
