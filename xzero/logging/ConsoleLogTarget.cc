c46958cd0c8dfdd806c1aaa0a5cbe1dd62a3349d
Christian Parpart <trapni@gmail.com>
2017-03-01 18:16:44 +0100

    [xzero] logging: also print microseconds in console logging target

--- xzero/logging/ConsoleLogTarget.cc
+++ xzero/logging/ConsoleLogTarget.cc
@@ -66,7 +66,13 @@ std::string ConsoleLogTarget::createTimestamp() const {
   if (timestampEnabled_ == false)
     return "";
 
-  return WallClock::now().toString("%Y-%m-%d %H:%M:%S ");
+  char buf[7];
+  UnixTime now = WallClock::now();
+  snprintf(buf, sizeof(buf), "%06lu", now.unixMicros() % 1000000);
+
+  return StringUtil::format("$0.$1 ",
+                            now.toString("%Y-%m-%d %H:%M:%S"),
+                            buf);
 }
 
 } // namespace xzero
