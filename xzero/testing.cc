48ca5bc78886e7bb9279ee37b6e2cc87467a5952
Christian Parpart <trapni@gmail.com>
2017-03-16 19:12:18 +0100

    [xzero] testing: adds better error reports on unhandled exceptions

--- xzero/testing.cc
+++ xzero/testing.cc
@@ -321,6 +321,21 @@ void UnitTest::runAllTestsOnce() {
       } catch (const BailOutException&) {
         // no-op
         failed++;
+      } catch (const RuntimeError& ex) {
+        reportMessage(
+            StringUtil::format(
+              "Unhandled exception caught in test. $0 ($1:$2 $3)",
+              ex.what(),
+              ex.sourceFile(),
+              ex.sourceLine(),
+              ex.functionName()),
+            false);
+        failed++;
+      } catch (const std::exception& ex) {
+        reportMessage(
+            StringUtil::format("Unhandled exception caught in test. $0", ex.what()),
+            false);
+        failed++;
       } catch (...) {
         reportMessage("Unhandled exception caught in test.", false);
         failed++;
