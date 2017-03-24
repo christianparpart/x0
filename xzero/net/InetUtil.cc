cb53a37eaf43ccb993367a4da0adab31239d95dd
Christian Parpart <trapni@gmail.com>
2017-03-24 18:00:04 +0100

    [xzero] InetUtil: adds some more generic INET helpers

--- xzero/net/InetUtil.cc
+++ xzero/net/InetUtil.cc
@@ -6,7 +6,10 @@
 // the License at: http://opensource.org/licenses/MIT
 
 #include <xzero/net/InetUtil.h>
+#include <xzero/io/FileUtil.h>
+#include <xzero/executor/Executor.h>
 #include <xzero/RuntimeError.h>
+#include <xzero/logging.h>
 
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
@@ -20,6 +23,8 @@
 #include <assert.h>
 
 namespace xzero {
+
+#define TRACE(msg...) logTrace("InetUtil", msg)
 
 int InetUtil::getLocalPort(int socket, int addressFamily) {
   switch (addressFamily) {
@@ -43,6 +48,79 @@ int InetUtil::getLocalPort(int socket, int addressFamily) {
       RAISE(IllegalStateError, "Invalid address family.");
     }
   }
+}
+
+Future<int> InetUtil::connect(const InetAddress& remote,
+                              Duration timeout,
+                              Executor* executor) {
+  Promise<int> promise;
+
+  int fd = socket(remote.family(), SOCK_STREAM, IPPROTO_TCP);
+  if (fd < 0) {
+    promise.failure(static_cast<std::errc>(errno));
+    return promise.future();
+  }
+
+  FileUtil::setBlocking(fd, false);
+
+  std::error_code ec = InetUtil::connect(fd, remote);
+
+  if (!ec) {
+    TRACE("InetUtil.connect: connected instantly");
+    promise.success(fd);
+  } else if (ec == std::errc::operation_in_progress) {
+    TRACE("InetUtil.connect: backgrounding");
+    executor->executeOnWritable(
+        fd,
+        [promise, fd]() { promise.success(fd); },
+        timeout,
+        [promise, fd]() { FileUtil::close(fd);
+                          promise.failure(std::errc::timed_out); });
+  } else {
+    TRACE("InetUtil.connect: failed. $0", ec.message());
+    promise.failure(ec);
+  }
+
+  return promise.future();
+}
+
+std::error_code InetUtil::connect(int fd, const InetAddress& remote) {
+  int rv;
+  switch (remote.family()) {
+    case AF_INET: {
+      struct sockaddr_in saddr;
+      memset(&saddr, 0, sizeof(saddr));
+      saddr.sin_family = remote.family();
+      saddr.sin_port = htons(remote.port());
+      memcpy(&saddr.sin_addr,
+             remote.ip().data(),
+             remote.ip().size());
+
+      TRACE("connectAsync: connect(ipv4)");
+      rv = ::connect(fd, (const struct sockaddr*) &saddr, sizeof(saddr));
+    }
+    case AF_INET6: {
+      struct sockaddr_in6 saddr;
+      memset(&saddr, 0, sizeof(saddr));
+      saddr.sin6_family = remote.family();
+      saddr.sin6_port = htons(remote.port());
+      memcpy(&saddr.sin6_addr,
+             remote.ip().data(),
+             remote.ip().size());
+
+      TRACE("connectAsync: connect(ipv6)");
+      rv = ::connect(fd, (const struct sockaddr*) &saddr, sizeof(saddr));
+      break;
+    }
+    default: {
+      return std::make_error_code(std::errc::invalid_argument);
+    }
+  }
+
+  if (rv < 0)
+    return std::make_error_code(static_cast<std::errc>(errno));
+  else
+    return std::error_code();
 }
 
 } // namespace xzero
--- xzero/net/InetUtil.h
+++ xzero/net/InetUtil.h
@@ -6,11 +6,26 @@
 // the License at: http://opensource.org/licenses/MIT
 #pragma once
 
+#include <xzero/thread/Future.h>
+#include <xzero/net/InetAddress.h>
+#include <xzero/Duration.h>
+#include <system_error>
+
 namespace xzero {
+
+class Executor;
 
 class InetUtil {
  public:
   static int getLocalPort(int socket, int addressFamily);
+
+  static Result<int> openTcpSocket(int addressFamily);
+
+  static Future<int> connect(const InetAddress& remote,
+                             Duration timeout,
+                             Executor* executor);
+
+  static std::error_code connect(int socket, const InetAddress& remote);
 };
 
 }  // namespace xzero
