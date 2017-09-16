70b955831dfcec76b6463a377da081bf1b634c07
Christian Parpart <christian@parpart.family>
2017-09-16 23:21:46 +0200

    [x0d] drops try/catch in main()

--- src/x0d/main.cc
+++ src/x0d/main.cc
@@ -66,113 +66,104 @@ PidFile::~PidFile() {
 
 int main(int argc, const char* argv[]) {
   std::unique_ptr<FileLogTarget> fileLogTarget;
-  try {
-    Application::init();
+  Application::init();
 
-    Flags flags;
-    flags.defineBool("help", 'h', "Prints this help and exits.")
-         .defineBool("version", 'v', "Prints software version and exits.")
-         .defineString("config", 'c', "PATH", "Specify a custom configuration file.", X0D_CONFIGFILE, nullptr)
-         .defineString("user", 'u', "NAME", "User privileges to drop down to.", Application::userName())
-         .defineString("group", 'g', "NAME", "Group privileges to drop down to.", Application::groupName())
-         .defineString("log-level", 'L', "ENUM", "Defines the minimum log level.", "info", nullptr)
-         .defineString("log-target", 0, "ENUM", "Specifies logging target. One of syslog, file, systemd, console.", "console", nullptr)
-         .defineString("log-file", 'l', "PATH", "Path to application log file.", "", nullptr)
-         .defineString("instant", 'i', "PATH[:PORT]", "Enable instant-mode (does not need config file).", "", nullptr)
-         .defineNumber("optimization-level", 'O', "LEVEL", "Sets the configuration optimization level.", 1)
-         .defineBool("daemonize", 'd', "Forks the process into background.")
-         .defineString("pid-file", 0, "PATH",
-                       "Path to PID-file this process will store its main PID.",
-                       X0D_PIDFILE,
-                       nullptr)
-         .defineBool("dump-ast", 0, "Dumps configuration AST and exits.")
-         .defineBool("dump-ir", 0, "Dumps configuration IR and exits.")
-         .defineBool("dump-tc", 0, "Dumps configuration opcode stream and exits.")
-         ;
+  Flags flags;
+  flags.defineBool("help", 'h', "Prints this help and exits.")
+       .defineBool("version", 'v', "Prints software version and exits.")
+       .defineString("config", 'c', "PATH", "Specify a custom configuration file.", X0D_CONFIGFILE, nullptr)
+       .defineString("user", 'u', "NAME", "User privileges to drop down to.", Application::userName())
+       .defineString("group", 'g', "NAME", "Group privileges to drop down to.", Application::groupName())
+       .defineString("log-level", 'L', "ENUM", "Defines the minimum log level.", "info", nullptr)
+       .defineString("log-target", 0, "ENUM", "Specifies logging target. One of syslog, file, systemd, console.", "console", nullptr)
+       .defineString("log-file", 'l', "PATH", "Path to application log file.", "", nullptr)
+       .defineString("instant", 'i', "[BIND:]:PORT", "Enable instant-mode (does not need config file).", "", nullptr)
+       .defineNumber("optimization-level", 'O', "LEVEL", "Sets the configuration optimization level.", 1)
+       .defineBool("daemonize", 'd', "Forks the process into background.")
+       .defineString("pid-file", 0, "PATH",
+                     "Path to PID-file this process will store its main PID.",
+                     X0D_PIDFILE,
+                     nullptr)
+       .defineBool("dump-ast", 0, "Dumps configuration AST and exits.")
+       .defineBool("dump-ir", 0, "Dumps configuration IR and exits.")
+       .defineBool("dump-tc", 0, "Dumps configuration opcode stream and exits.")
+       ;
 
-    std::error_code ec = flags.parse(argc, argv);
-    if (ec) {
-      fprintf(stderr, "Failed to parse flags. %s\n", ec.message().c_str());
-      return 1;
-    }
-
-    if (flags.getBool("help")) {
-      printHelp(flags);
-      return 0;
-    }
-
-    if (flags.getBool("version")) {
-      printVersion();
-      return 0;
-    }
-
-    x0d::XzeroDaemon x0d;
-
-    // {{{ setup logging
-    Logger::get()->setMinimumLogLevel(make_loglevel(flags.getString("log-level")));
-
-    std::string logTarget = flags.getString("log-target");
-    if (logTarget == "null") {
-      ; // ignore
-    } else if (logTarget == "console") {
-      Logger::get()->addTarget(ConsoleLogTarget::get());
-    } else if (logTarget == "file") {
-      std::string filename = flags.getString("log-file");
-      std::shared_ptr<File> file = x0d.vfs().getFile(filename);
-      File::OpenFlags openFlags = File::Write | File::Create | File::Append;
-      std::unique_ptr<OutputStream> out = file->createOutputChannel(openFlags);
-      fileLogTarget.reset(new FileLogTarget(std::move(out)));
-      Logger::get()->addTarget(fileLogTarget.get());
-    } else if (logTarget == "syslog") {
-      ; // TODO Logger::get()->addTarget(SyslogTarget::get());
-    } else if (logTarget == "systemd") {
-      ; // TODO Logger::get()->addTarget(SystemdLogTarget::get());
-    } else {
-      fprintf(stderr, "Invalid log target \"%s\".\n", logTarget.c_str());
-      return 1;
-    }
-    // }}}
-
-    x0d.setOptimizationLevel(flags.getNumber("optimization-level"));
-
-    bool dumpAST = flags.getBool("dump-ast");
-    bool dumpIR = flags.getBool("dump-ir");
-    bool dumpTC = flags.getBool("dump-tc");
-
-    std::shared_ptr<xzero::flow::vm::Program> config =
-        x0d.loadConfigFile(flags.getString("config"), dumpAST, dumpIR, dumpTC);
-
-    bool exitBeforeRun = dumpAST || dumpIR || dumpTC;
-    if (exitBeforeRun)
-      return 0;
-
-    if (!x0d.applyConfiguration(config))
-      return 1;
-
-    std::string pidfilepath = FileUtil::absolutePath(flags.getString("pid-file"));
-    std::string pidfiledir = FileUtil::dirname(pidfilepath);
-    if (!FileUtil::exists(pidfiledir)) {
-      FileUtil::mkdir_p(pidfiledir);
-      FileUtil::chown(pidfiledir, flags.getString("user"),
-                                  flags.getString("group"));
-    }
-
-    Application::dropPrivileges(flags.getString("user"), flags.getString("group"));
-
-    if (flags.getBool("daemonize")) {
-      Application::daemonize();
-    }
-
-    PidFile pidFile = pidfilepath;
-
-    x0d.run();
-  } catch (const xzero::RuntimeError& e) {
-    e.debugPrint(&std::cerr);
-    return 1;
-  } catch (const std::exception& e) {
-    fprintf(stderr, "Unhandled exception caught (%s). %s\n", typeid(e).name(), e.what());
+  std::error_code ec = flags.parse(argc, argv);
+  if (ec) {
+    fprintf(stderr, "Failed to parse flags. %s\n", ec.message().c_str());
     return 1;
   }
 
+  if (flags.getBool("help")) {
+    printHelp(flags);
+    return 0;
+  }
+
+  if (flags.getBool("version")) {
+    printVersion();
+    return 0;
+  }
+
+  x0d::XzeroDaemon x0d;
+
+  // {{{ setup logging
+  Logger::get()->setMinimumLogLevel(make_loglevel(flags.getString("log-level")));
+
+  std::string logTarget = flags.getString("log-target");
+  if (logTarget == "null") {
+    ; // ignore
+  } else if (logTarget == "console") {
+    Logger::get()->addTarget(ConsoleLogTarget::get());
+  } else if (logTarget == "file") {
+    std::string filename = flags.getString("log-file");
+    std::shared_ptr<File> file = x0d.vfs().getFile(filename);
+    File::OpenFlags openFlags = File::Write | File::Create | File::Append;
+    std::unique_ptr<OutputStream> out = file->createOutputChannel(openFlags);
+    fileLogTarget.reset(new FileLogTarget(std::move(out)));
+    Logger::get()->addTarget(fileLogTarget.get());
+  } else if (logTarget == "syslog") {
+    ; // TODO Logger::get()->addTarget(SyslogTarget::get());
+  } else if (logTarget == "systemd") {
+    ; // TODO Logger::get()->addTarget(SystemdLogTarget::get());
+  } else {
+    fprintf(stderr, "Invalid log target \"%s\".\n", logTarget.c_str());
+    return 1;
+  }
+  // }}}
+
+  x0d.setOptimizationLevel(flags.getNumber("optimization-level"));
+
+  bool dumpAST = flags.getBool("dump-ast");
+  bool dumpIR = flags.getBool("dump-ir");
+  bool dumpTC = flags.getBool("dump-tc");
+
+  std::shared_ptr<xzero::flow::vm::Program> config =
+      x0d.loadConfigFile(flags.getString("config"), dumpAST, dumpIR, dumpTC);
+
+  bool exitBeforeRun = dumpAST || dumpIR || dumpTC;
+  if (exitBeforeRun)
+    return 0;
+
+  if (!x0d.applyConfiguration(config))
+    return 1;
+
+  std::string pidfilepath = FileUtil::absolutePath(flags.getString("pid-file"));
+  std::string pidfiledir = FileUtil::dirname(pidfilepath);
+  if (!FileUtil::exists(pidfiledir)) {
+    FileUtil::mkdir_p(pidfiledir);
+    FileUtil::chown(pidfiledir, flags.getString("user"),
+                                flags.getString("group"));
+  }
+
+  Application::dropPrivileges(flags.getString("user"), flags.getString("group"));
+
+  if (flags.getBool("daemonize")) {
+    Application::daemonize();
+  }
+
+  PidFile pidFile = pidfilepath;
+
+  x0d.run();
   return 0;
 }
