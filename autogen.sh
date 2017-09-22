3f4de5168605cdd1d446391dba8d6deb8a05c984
Christian Parpart <christian@parpart.family>
2017-09-22 17:25:40 +0200

    autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -24,12 +24,6 @@ if test "$1" == "clean"; then
   exit 0
 fi
 
-if [[ -d "/usr/local/opt/llvm/bin" ]]; then
-  export PATH="/usr/local/opt/llvm/bin:${PATH}"
-  export CXXFLAGS="$CXXFLAGS -nostdinc++ -I/usr/local/opt/llvm/include/c++/v1"
-  export LDFLAGS="$LDFLAGS -L/usr/local/opt/llvm/lib"
-fi
-
 findexe() {
   for exe in ${@}; do
     if which $exe 2>/dev/null; then
@@ -39,6 +33,15 @@ findexe() {
   echo $1
 }
 
+# Mac OSX has a special location for more recent LLVM/clang installations
+#   $ brew tap homebrew/versions
+#   $ brew install llvm
+if [[ -d "/usr/local/opt/llvm/bin" ]]; then
+  export PATH="/usr/local/opt/llvm/bin:${PATH}"
+  export CXXFLAGS="$CXXFLAGS -nostdinc++ -I/usr/local/opt/llvm/include/c++/v1"
+  export LDFLAGS="$LDFLAGS -L/usr/local/opt/llvm/lib"
+fi
+
 # Mac OS/X has `brew install zlib`'d its zlib.pc somewhere non-standard ;-)
 pkgdirs=( "/usr/local/opt/zlib/lib/pkgconfig" )
 for pkgdir in ${pkgdirs[*]}; do
@@ -47,8 +50,8 @@ for pkgdir in ${pkgdirs[*]}; do
   fi
 done
 
-export CXX=$(findexe $CXX clang++-4.0 clang++ g++)
-export CC=$(findexe $CC clang-4.0 clang gcc)
+export CXX=$(findexe $CXX clang++-5.0 clang++-4.0 clang++ g++)
+export CC=$(findexe $CC clang++-5.0 clang-4.0 clang gcc)
 export CXXFLAGS="-O0 -g"
 
 echo CXX = $CXX
